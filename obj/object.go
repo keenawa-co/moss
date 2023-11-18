f6329f4bdcf5e5b78ae7f4b94be76e1dfddcd8d3
g10z3r <20g10z3r@gmail.com>
2023-11-18 11:37:05 +0100

    Picker cleaning

--- obj/object.go
+++ obj/object.go
@@ -1,12 +1,8 @@
 package obj
 
 import (
-	"bytes"
-	"fmt"
 	"go/ast"
-	"go/format"
 	"go/token"
-	"reflect"
 )
 
 const NoPos int = 0
@@ -40,18 +36,6 @@ var objKindStrings = [...]string{
 }
 
 func (kind ObjKind) String() string { return objKindStrings[kind] }
-
-// objKindFromString function is necessary for cases when there is *ast.Object
-// and it is necessary to convert it to a local Object
-func objKindFromString(value string) ObjKind {
-	for i, v := range objKindStrings {
-		if v == value {
-			return ObjKind(i)
-		}
-	}
-
-	return Bad
-}
 
 type Object interface {
 	Kind() ObjKind
@@ -141,90 +125,6 @@ func (f *FieldObjList) Len() int {
 	}
 
 	return n
-}
-
-func determineExprType(fobj *FileObj, expr ast.Expr, adder func(filed *FieldObj)) (any, error) {
-	switch e := expr.(type) {
-	case *ast.StructType:
-		return NewStructTypeObj(fobj, e)
-
-	case *ast.SelectorExpr:
-		ident, ok := e.X.(*ast.Ident)
-		if !ok {
-			return e.Sel.Name, nil
-		}
-
-		if _, exists := fobj.Imports.Meta[ident.Name]; exists {
-			adder(&FieldObj{
-				Names: []*IdentObj{{Name: ident.Name}},
-				Type:  e.Sel.Name,
-			})
-		}
-
-		return e.Sel.Name, nil
-
-	case *ast.StarExpr:
-		// This branch, in my understanding, is used only to determine the receiver of methods.
-		// Getting into this branch means that receiver's type is a pointer.
-
-		// attempt to get the name of the struct
-		if ident, ok := e.X.(*ast.Ident); ok {
-			return ident.Name, nil
-		}
-
-		return nil, fmt.Errorf("failed to get expr %s type", reflect.TypeOf(e.X))
-
-	case *ast.Ident:
-		// This branch, in my understanding, is used only to determine the receiver of methods.
-		// Getting into this branch means that receiver's type is not a pointer, it's a regular struct.
-		// So return the name of the struct.
-		return e.Name, nil
-
-	default:
-		var buf bytes.Buffer
-		if err := format.Node(&buf, fobj.FileSet, e); err != nil {
-			return nil, fmt.Errorf("failed to format expr: %w", err)
-		}
-
-		return buf.String(), nil
-	}
-}
-
-func processField(fobj *FileObj, field *ast.Field, adder func(filed *FieldObj)) (*FieldObj, error) {
-	typ, err := determineExprType(fobj, field.Type, adder)
-	if err != nil {
-		return nil, err
-	}
-
-	names := make([]*IdentObj, 0, len(field.Names))
-	for _, name := range field.Names {
-		names = append(names, &IdentObj{
-			Name: name.Name,
-			Kind: objKindFromString(name.Obj.Kind.String()),
-		})
-	}
-
-	return &FieldObj{
-		Names: names,
-		Type:  typ,
-	}, nil
-}
-
-func ProcessFieldList(fobj *FileObj, fieldList *ast.FieldList, adder func(filed *FieldObj)) (*FieldObjList, error) {
-	fieldObjList := &FieldObjList{
-		List: make([]*FieldObj, 0, len(fieldList.List)),
-	}
-
-	for _, field := range fieldList.List {
-		fieldObj, err := processField(fobj, field, adder)
-		if err != nil {
-			return nil, err
-		}
-
-		fieldObjList.List = append(fieldObjList.List, fieldObj)
-	}
-
-	return fieldObjList, nil
 }
 
 // ----------------------------------------------------------------------------
--- picker.go
+++ picker.go
@@ -1,7 +1,11 @@
 package compass
 
 import (
+	"errors"
+	"fmt"
 	"go/ast"
+	"reflect"
+	"strings"
 
 	"github.com/4rchr4y/go-compass/obj"
 )
@@ -27,4 +31,169 @@ type picker struct {
 
 func (a *picker) Pick(s *State, n ast.Node) (obj.Object, error) {
 	return a.pick(s, n)
+}
+
+// ----------------------------------------------------------------------------
+// Types pickers
+//
+// The creation of pickers, which allow for the extraction and analysis of
+// specific data types (structures and functional types) from an AST
+// (Abstract Syntax Tree), representing the structure of the source code.
+
+func NewStructTypePicker() Picker {
+	return NewPicker(pickStructType)
+}
+
+func pickStructType(state *State, node ast.Node) (obj.Object, error) {
+	typeSpec, ok := node.(*ast.TypeSpec)
+	if !ok {
+		return nil, fmt.Errorf("some error from pickStructType : %s not a *ast.TypeSpec", reflect.TypeOf(node).String()) // TODO: add normal error return message
+	}
+
+	structType, ok := typeSpec.Type.(*ast.StructType)
+	if !ok {
+		return nil, fmt.Errorf("some error from pickStructType : %s not a *ast.StructType", reflect.TypeOf(node).String()) // TODO: add normal error return message
+	}
+
+	typeObj := new(obj.TypeObj)
+
+	if typeSpec.TypeParams != nil && len(typeSpec.TypeParams.List) > 0 {
+		var err error
+		typeObj.TypeParams, err = obj.ProcessFieldList(state.File, typeSpec.TypeParams, typeObj.ImportAdder)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	structTypeObj, err := obj.NewStructTypeObj(state.File, structType)
+	if err != nil {
+		return nil, errors.New("some error from pickStructType 3") // TODO: add normal error return message
+	}
+
+	typeObj.Type = structTypeObj
+	typeObj.Pos = state.File.FileSet.Position(typeSpec.Pos()).Line
+	typeObj.End = state.File.FileSet.Position(typeSpec.End()).Line
+	typeObj.Name = &obj.IdentObj{
+		Name: typeSpec.Name.Name,
+		Kind: obj.Typ,
+	}
+
+	return typeObj, nil
+}
+
+func NewFuncTypePicker() Picker {
+	return NewPicker(pickFuncType)
+}
+
+func pickFuncType(state *State, node ast.Node) (obj.Object, error) {
+	ts, ok := node.(*ast.TypeSpec)
+	if !ok {
+		return nil, fmt.Errorf("some error from pickFuncType : %s not a *ast.TypeSpec", reflect.TypeOf(node).String()) // TODO: add normal error return message
+	}
+
+	funcType, ok := ts.Type.(*ast.FuncType)
+	if !ok {
+		return nil, fmt.Errorf("some error from pickFuncType : %s not a *ast.StructType", reflect.TypeOf(node).String()) // TODO: add normal error return message
+	}
+
+	typeObj := new(obj.TypeObj)
+
+	if ts.TypeParams != nil && len(ts.TypeParams.List) > 0 {
+		var err error
+		typeObj.TypeParams, err = obj.ProcessFieldList(state.File, ts.TypeParams, typeObj.ImportAdder)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	funcTypeObj, err := obj.NewFuncTypeObj(state.File, funcType)
+	if err != nil {
+		return nil, errors.New("some error from pickFuncType 3") // TODO: add normal error return message
+	}
+
+	typeObj.Pos = state.File.FileSet.Position(ts.Pos()).Line
+	typeObj.End = state.File.FileSet.Position(ts.End()).Line
+	typeObj.Type = funcTypeObj
+	typeObj.Name = &obj.IdentObj{
+		Name: ts.Name.Name,
+		Kind: obj.Typ,
+	}
+
+	return typeObj, nil
+}
+
+// ----------------------------------------------------------------------------
+// Declaration pickers
+//
+// Identifying and extracting declarations from an AST
+// (Abstract Syntax Tree) of Go source code.
+
+func NewFuncDeclPicker() Picker {
+	return NewPicker(pickFuncDecl)
+}
+
+func pickFuncDecl(state *State, node ast.Node) (obj.Object, error) {
+	decl, _ := node.(*ast.FuncDecl)
+
+	funcDeclObj, err := obj.NewFuncDeclObj(state.File, decl)
+	if err != nil {
+		return nil, errors.New("some error from pickFuncDecl 1") // TODO: add normal error return message
+	}
+
+	return &obj.DeclObj{
+		Pos: state.File.FileSet.Position(decl.Pos()).Line,
+		End: state.File.FileSet.Position(decl.End()).Line,
+		Name: &obj.IdentObj{
+			Name: decl.Name.Name,
+			Kind: obj.Fun,
+		},
+		Type: funcDeclObj,
+	}, nil
+}
+
+// ----------------------------------------------------------------------------
+// Specs pickers
+//
+// Identifying and extracting declarations from an AST
+// (Abstract Syntax Tree) of Go source code.
+
+func NewImportSpecPicker() Picker {
+	return NewPicker(pickImportSpec)
+}
+
+func pickImportSpec(state *State, node ast.Node) (obj.Object, error) {
+	importSpec, ok := node.(*ast.ImportSpec)
+	if !ok {
+		return nil, errors.New("node is not an ImportSpec")
+	}
+
+	if importSpec.Path == nil {
+		return nil, errors.New("import spec path is nil")
+	}
+
+	// from ast get an import string that is wrapped in quotes
+	path := strings.Trim(importSpec.Path.Value, `"`)
+	hasModPrefix := strings.HasPrefix(path, state.Modfile.Module.Mod.Path)
+	if hasModPrefix {
+		path = path[len(state.Modfile.Module.Mod.Path):]
+	}
+
+	if importSpec.Name != nil && importSpec.Name.Name == "_" {
+		importObj := obj.NewImportObj(importSpec, obj.SideEffect)
+		importObj.Path = path
+
+		return importObj, nil
+	}
+
+	if !hasModPrefix {
+		importObj := obj.NewImportObj(importSpec, obj.External)
+		importObj.Path = path
+
+		return importObj, nil
+	}
+
+	importObj := obj.NewImportObj(importSpec, obj.Internal)
+	importObj.Path = path
+
+	return importObj, nil
 }
