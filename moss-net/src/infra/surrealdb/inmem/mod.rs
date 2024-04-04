ea12e4572b751517b1fe93eed2af6063686045ed
g10z3r <20g10z3r@gmail.com>
2024-04-04 19:49:05 +0200

    user_repo removed

--- moss-net/src/infra/surrealdb/inmem/mod.rs
+++ moss-net/src/infra/surrealdb/inmem/mod.rs
@@ -1,23 +1,9 @@
-use async_graphql::SimpleObject;
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use surrealdb::sql::Thing;
 use surrealdb::{engine::local::Db, Surreal};
 
-use crate::err::Error;
-
-mod user_repo;
-
-pub struct SurrealInMem {
-    // pub user_repo: UserRepository,
-}
+pub struct SurrealInMem {}
 
 impl SurrealInMem {
     pub fn new(client: Surreal<Db>) -> Self {
-        Self {
-            // user_repo: UserRepository::new(client),
-        }
+        Self {}
     }
 }
