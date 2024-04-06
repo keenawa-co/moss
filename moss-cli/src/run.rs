cb0ca9bdf1c9629601e58c847f9791257464cffd
g10z3r <20g10z3r@gmail.com>
2024-04-06 10:45:55 +0200

    Removing db dead code

--- moss-cli/src/run.rs
+++ moss-cli/src/run.rs
@@ -16,30 +16,6 @@ pub async fn init(RunCmdArgs { bind, ubp_path }: RunCmdArgs) -> anyhow::Result<(
     let preference = loader::load_preference_file(ubp_path)?;
     let _ = moss_net::CONF.set(moss_net::Config { bind, preference });
 
-    // let inmemdb = prepare_inmemdb(ubp_path).await?;
-
     moss_net::bind().await.expect("Failed to start the server");
     return Ok(());
 }
-
-// async fn prepare_inmemdb(ubp_filepath: String) -> anyhow::Result<SurrealInMem> {
-//     let db = Surreal::new::<Mem>(()).await?;
-//     db.use_ns(core::constant::APP_NAME)
-//         .use_db(server::infra::DEFAULT_INMEM_DB_NAME)
-//         .await?;
-
-//     let inmemdb = server::infra::surrealdb::inmem::SurrealInMem::new(db);
-
-//     let data = load_ubp_config(ubp_filepath)?;
-
-//     {
-//         for category in data {
-//             inmemdb
-//                 .user_repo
-//                 .create_preference_category(category)
-//                 .await?;
-//         }
-//     }
-
-//     Ok(inmemdb)
-// }
