8439a96fe9209c511c447a76ec06e0bfea7d326b
g10z3r <20g10z3r@gmail.com>
2024-04-16 20:39:32 +0200

    Updated initial schema

--- migration/m20220101_000001_initial_schema.up.sql
+++ migration/m20220101_000001_initial_schema.up.sql
@@ -1,6 +1,28 @@
-CREATE TABLE IF NOT EXISTS project (
+CREATE TABLE project (
     id VARCHAR(20) PRIMARY KEY NOT NULL,
     source TEXT UNIQUE NOT NULL,
     last_used_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE session (
+    id VARCHAR(20) PRIMARY KEY NOT NULL,
+    project_id VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY(project_id) REFERENCES project(id)
+);
+
+CREATE TABLE metric_feed (
+    id VARCHAR(20) PRIMARY KEY NOT NULL,
+    session_id VARCHAR(20) NOT NULL,
+    project_id VARCHAR(20) NOT NULL,
+    feed JSON,
+    chunk_num INT NOT NULL,
+    chunk_total INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY(session_id) REFERENCES session(id),
+    FOREIGN KEY(project_id) REFERENCES project(id)
+);
+
+CREATE INDEX idx_session_id ON metric_feed(session_id);
+CREATE INDEX idx_project_id ON metric_feed(project_id);
