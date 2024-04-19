f3b272aafc7234a18222efa77eb68c7b8de2cdcc
g10z3r <20g10z3r@gmail.com>
2024-04-19 16:43:28 +0200

    Deep refactoring

--- kernel/sys/src/lib.rs
+++ kernel/sys/src/lib.rs
@@ -1,88 +1,76 @@
+use anyhow::anyhow;
+use chan::BroadcastChannel;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::{
-    path::{Path, PathBuf},
+    path::PathBuf,
     sync::{Arc, Mutex},
 };
-
-use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
 use tokio::sync::broadcast;
 
-pub struct Channel<T>
-where
-    T: Clone,
-{
-    tx: broadcast::Sender<T>,
-}
-
-impl<T> Channel<T>
-where
-    T: Clone,
-{
-    pub fn new(capacity: usize) -> Self {
-        let (tx, _) = broadcast::channel(capacity);
-        Channel { tx }
-    }
-
-    pub fn subscribe(&self) -> broadcast::Receiver<T> {
-        self.tx.subscribe()
-    }
-}
+pub type Config = notify::Config;
 
 pub struct FileWatcher {
-    channel: Channel<f32>,
+    channel: Arc<BroadcastChannel<f32>>,
     watcher: Mutex<Option<RecommendedWatcher>>,
-    status: Mutex<bool>,
+    subscriber_count: Mutex<usize>,
     watch_list: Mutex<Vec<PathBuf>>,
 }
 
 impl FileWatcher {
     pub fn new() -> Arc<Self> {
         Arc::new(Self {
-            channel: Channel::new(32),
+            channel: Arc::new(BroadcastChannel::new(32)),
             watcher: Mutex::new(None),
-            status: Mutex::new(false),
+            subscriber_count: Mutex::new(0),
             watch_list: Mutex::new(Vec::new()),
         })
     }
 
     pub fn subscribe(self: &Arc<Self>) -> anyhow::Result<broadcast::Receiver<f32>> {
-        println!("Subscribed!");
+        let mut subscriber_count = self
+            .subscriber_count
+            .lock()
+            .map_err(|e| anyhow!(e.to_string()))?;
+        *subscriber_count += 1;
 
-        let mut status = self.status.lock().unwrap();
-        if !*status {
-            *status = true;
-            drop(status); // release lock before calling `run`
+        // run the watcher if this is the first subscriber
+        if *subscriber_count == 1 {
+            drop(subscriber_count);
             self.run()?;
         }
 
         Ok(self.channel.subscribe())
     }
 
-    pub fn register_path(self: &Arc<Self>, path: PathBuf) -> anyhow::Result<()> {
-        let mut paths = self.watch_list.lock().unwrap();
-        if !paths.contains(&path) {
-            paths.push(path.clone());
+    pub fn unsubscribe(self: &Arc<Self>) -> anyhow::Result<()> {
+        let mut subscriber_count = self
+            .subscriber_count
+            .lock()
+            .map_err(|e| anyhow!(e.to_string()))?;
+        if *subscriber_count > 0 {
+            *subscriber_count -= 1;
         }
 
-        // Automatically start watching new path if watcher is already running
-        if *self.status.lock().unwrap() {
-            self.add_path_to_watcher(&path)?;
+        // stop the watcher if this is the last subscriber
+        if *subscriber_count == 0 {
+            self.stop()?;
         }
 
         Ok(())
     }
 
-    fn add_path_to_watcher(&self, path: &PathBuf) -> anyhow::Result<()> {
-        let mut watcher_guard = self.watcher.lock().unwrap();
-        if let Some(watcher) = watcher_guard.as_mut() {
-            watcher.watch(path, RecursiveMode::Recursive)?;
+    pub fn watch_path(self: &Arc<Self>, path: &PathBuf) -> anyhow::Result<()> {
+        let mut list_lock = self.watch_list.lock().map_err(|e| anyhow!(e.to_string()))?;
+        if !list_lock.contains(path) {
+            list_lock.push(path.clone());
+            self.create_watcher_path(path)?;
         }
+
         Ok(())
     }
 
-    fn run(self: &Arc<Self>) -> anyhow::Result<()> {
-        let initial_watch_list = self.watch_list.lock().unwrap().clone();
-        let tx_clone = self.channel.tx.clone();
-
+    fn create_watcher(&self) -> anyhow::Result<RecommendedWatcher> {
+        let chan = self.channel.clone();
         let watcher = RecommendedWatcher::new(
             move |res| {
                 futures::executor::block_on(async {
@@ -90,7 +78,7 @@ impl FileWatcher {
                         Ok(event) => {
                             println!("Event detected: {:?}", event);
 
-                            if let Err(e) = tx_clone.send(0.5) {
+                            if let Err(e) = chan.send(0.5) {
                                 eprintln!("Error sending event: {:?}", e);
                             }
                         }
@@ -101,46 +89,42 @@ impl FileWatcher {
             Config::default(),
         )?;
 
-        let mut watcher_lock = self.watcher.lock().unwrap();
-        *watcher_lock = Some(watcher);
+        Ok(watcher)
+    }
 
-        for path in initial_watch_list {
-            self.add_path_to_watcher(&path)?;
+    fn create_watcher_path(&self, path: &PathBuf) -> anyhow::Result<()> {
+        let mut watcher_lock = self.watcher.lock().map_err(|e| anyhow!(e.to_string()))?;
+        if let Some(ref mut watcher) = *watcher_lock {
+            watcher.watch(path, RecursiveMode::Recursive)?;
+        }
+
+        Ok(())
+    }
+
+    fn run(self: &Arc<Self>) -> anyhow::Result<()> {
+        let mut watcher_lock = self.watcher.lock().map_err(|e| anyhow!(e.to_string()))?;
+        if watcher_lock.is_none() {
+            *watcher_lock = Some(self.create_watcher()?);
+        }
+
+        let paths = self.watch_list.lock().map_err(|e| anyhow!(e.to_string()))?;
+        for path in paths.iter() {
+            self.create_watcher_path(&path)?;
+        }
+
+        Ok(())
+    }
+
+    fn stop(self: &Arc<Self>) -> anyhow::Result<()> {
+        let paths = self.watch_list.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut watcher_lock = self.watcher.lock().map_err(|e| anyhow!(e.to_string()))?;
+
+        if let Some(watcher) = watcher_lock.as_mut() {
+            for path in paths.iter() {
+                watcher.unwatch(&path)?;
+            }
         }
 
         Ok(())
     }
 }
-
-// impl FileWatcher {
-//     fn run(&mut self) -> anyhow::Result<()> {
-//         let tx_clone = self.channel.tx.clone();
-
-//         let watcher = RecommendedWatcher::new(
-//             move |res| {
-//                 dbg!(&res);
-
-//                 futures::executor::block_on(async {
-//                     match res {
-//                         Ok(event) => {
-//                             println!("Event detected: {:?}", event);
-//                             let ev = EventType::FileEvent(FileEventType::ChangedEvent(
-//                                 ChangedEventPayload { testdata: 0.5 },
-//                             ));
-
-//                             if let Err(e) = tx_clone.send(ev).await {
-//                                 eprintln!("Error sending event: {:?}", e);
-//                             }
-//                         }
-//                         Err(e) => eprintln!("Watch error: {:?}", e),
-//                     }
-//                 })
-//             },
-//             notify::Config::default(),
-//         )?;
-
-//         self.watcher = Some(watcher);
-
-//         Ok(())
-//     }
-// }
