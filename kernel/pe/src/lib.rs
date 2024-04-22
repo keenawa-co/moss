0e94d2f224a9845936fff2e68f6e3c6f67fd2b79
g10z3r <20g10z3r@gmail.com>
2024-04-22 22:05:59 +0200

    .

--- kernel/pe/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod engine;
-pub mod policy;
-
-#[macro_use]
-extern crate anyhow;
-
-#[macro_use]
-extern crate serde;
--- kernel/pe/src/policy.rs
+++
@@ -1,6 +0,0 @@
-use async_graphql::SimpleObject;
-
-#[derive(Clone, Serialize, Deserialize, Debug, SimpleObject)]
-pub struct Report {
-    pub source: String,
-}
