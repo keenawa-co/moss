6f6c1beedf29515050e430540e9e5a6fa7a46b4c
g10z3r <20g10z3r@gmail.com>
2024-04-29 20:11:48 +0200

    Documentation and one new macro

--- lib/mac/src/defer.rs
+++ lib/mac/src/defer.rs
@@ -1,5 +1,33 @@
 use core::mem::ManuallyDrop;
 
+/// Executes a provided function when the returned `Defer` guard object is dropped.
+/// This is typically used to ensure that cleanup code is executed when a scope is exited,
+/// either normally or via a panic.
+///
+/// # Type Parameters
+/// - `F`: The type of the closure to execute. Must implement `FnOnce()`.
+///
+/// # Parameters
+/// - `f`: A closure of type `F` that is called when the `Defer` object is dropped.
+///
+/// # Returns
+/// - A `Defer<F>` guard object that calls the provided closure upon being dropped.
+///
+/// # Examples
+/// ```
+/// use your_crate_name::defer;
+///
+/// fn main() {
+///     let resource = "Resource allocation here";
+///     let _defer = defer(|| println!("Cleanup code for {}", resource));
+///     println!("Main logic here");
+///     // When `_defer` is dropped at the end of `main`, the closure is called.
+/// }
+/// ```
+///
+/// The function ensures that the cleanup code is run no matter how the scope is exited,
+/// which is particularly useful for managing resources or handling cleanup tasks.
+
 pub fn defer<F>(f: F) -> impl Drop
 where
     F: FnOnce(),
@@ -15,6 +43,29 @@ where
 
     Defer(ManuallyDrop::new(f))
 }
+
+/// A convenience macro to create a `Defer` object for running cleanup or other
+/// tasks when the current scope is exited. This macro simplifies the usage of
+/// the `defer` function by automatically creating a `Defer` object.
+///
+/// # Syntax
+/// `defer!(expression);`
+///
+/// # Parameters
+/// - `expression`: An expression or closure to execute upon scope exit.
+///
+/// # Examples
+/// ```
+/// use your_crate_name::defer;
+///
+/// fn main() {
+///     defer!(println!("This will be printed on scope exit"));
+///     println!("This is printed first");
+/// }
+/// ```
+///
+/// Using the `defer!` macro, you can ensure that specific actions are taken
+/// at the end of a scope, similar to destructors or finally blocks in other languages.
 
 #[macro_export]
 macro_rules! defer {
--- lib/mac/src/lib.rs
+++ lib/mac/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod defer;
+pub mod maybe;
