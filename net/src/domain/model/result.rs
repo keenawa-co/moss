82aacc70b6cab159d7539edc553266f6b519474c
g10z3r <20g10z3r@gmail.com>
2024-05-01 13:30:43 +0200

    Error enum refactoring

--- net/src/domain/model/result.rs
+++ net/src/domain/model/result.rs
@@ -1,51 +1,108 @@
-use anyhow::Error as AnyhowError;
-use axum::Error as AxumError;
-use notify::Error as NotifyError;
-use sea_orm::DbErr as SeaOrmDbError;
-use serde_json::Error as SerdeError;
-use std::io::Error as IoError;
 use thiserror::Error;
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
 
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("{0}")]
-    Notify(#[from] NotifyError),
-
-    #[error("{0}")]
-    Anyhow(#[from] AnyhowError),
-
-    #[error("Invalid request body")]
-    Request,
-
-    #[error("Network error: {0}")]
-    Axum(#[from] AxumError),
-
-    #[error("Serialization/deserialization error: {0}")]
-    Serde(#[from] SerdeError),
-
-    #[error("Database operation failed: {0}")]
-    Database(
-        #[source]
-        #[from]
-        SeaOrmDbError,
-    ),
-
-    #[error("File access error: {0}")]
-    Io(#[from] IoError),
-
-    #[error("Internal error: {0}")]
-    Internal(String),
-}
-
 #[macro_export]
 macro_rules! internal {
     ($err:expr) => {{
-        $crate::domain::Error::Internal(format!("{}:{}: {}", file!(), line!(), $err))
+        $crate::domain::Error::Internal(
+            $crate::domain::model::result::InternalError::Unknown(format!("{}:{}: {}", file!(), line!(), $err)),
+        )
     }};
 
     ($fmt:expr, $($arg:expr),*) => {{
-        $crate::domain::Error::Internal(format!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg),*)))
+        $crate::domain::Error::Internal(
+            $crate::domain::model::result::InternalError::Unknown(format!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg),*))),
+        )
     }};
+}
+
+#[macro_export]
+macro_rules! bad {
+    ($err:expr) => {{
+        $crate::domain::Error::Client(
+            $crate::domain::model::result::ClientError::BadRequest(format!("{}:{}: {}", file!(), line!(), $err)),
+        )
+    }};
+
+    ($fmt:expr, $($arg:expr),*) => {{
+        $crate::domain::Error::Client(
+            $crate::domain::model::result::ClientError::BadRequest(format!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg),*))),
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! not_found {
+    ($err:expr) => {{
+        $crate::domain::Error::Client(
+            $crate::domain::model::result::ClientError::NotFound(format!("{}:{}: {}", file!(), line!(), $err)),
+        )
+    }};
+
+    ($fmt:expr, $($arg:expr),*) => {{
+        $crate::domain::Error::Client(
+            $crate::domain::model::result::ClientError::NotFound(format!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg),*))),
+        )
+    }};
+}
+
+macro_rules! transparent_error {
+    ($outer_variant:ident, $inner_variant:path, $err_type:ty) => {
+        impl From<$err_type> for Error {
+            fn from(err: $err_type) -> Self {
+                Error::$outer_variant($inner_variant(err))
+            }
+        }
+    };
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Client(ClientError),
+
+    #[error(transparent)]
+    Internal(InternalError),
+}
+
+transparent_error!(Internal, InternalError::Database, sea_orm::DbErr);
+transparent_error!(Internal, InternalError::Io, std::io::Error);
+transparent_error!(Internal, InternalError::Anyhow, anyhow::Error);
+transparent_error!(Internal, InternalError::Notify, notify::Error);
+transparent_error!(Internal, InternalError::Axum, axum::Error);
+
+transparent_error!(Client, ClientError::Serde, serde_json::Error);
+
+#[derive(Error, Debug)]
+pub enum ClientError {
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+
+    #[error("Cannot or will not process the request: {0}")]
+    BadRequest(String),
+
+    #[error("Cannot find the requested resource: {0}")]
+    NotFound(String),
+}
+
+#[derive(Error, Debug)]
+pub enum InternalError {
+    #[error("Unknown server error: {0}")]
+    Unknown(String),
+
+    #[error(transparent)]
+    Database(#[from] sea_orm::DbErr),
+
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Axum(#[from] axum::Error),
+
+    #[error(transparent)]
+    Notify(#[from] notify::Error),
+
+    #[error(transparent)]
+    Anyhow(#[from] anyhow::Error),
 }
