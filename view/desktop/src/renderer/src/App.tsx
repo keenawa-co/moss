232253032af24edf83ed57a9c39b212f3a8fa767
jevgenis33 <jevgenijs.semjonovs@nagra.com>
2024-06-09 12:52:15 +0200

    Added DraggableTopBar to render app.

--- view/desktop/src/renderer/src/App.tsx
+++ view/desktop/src/renderer/src/App.tsx
@@ -1,11 +1,14 @@
-import { Content, RootLayout, Sidebar } from "@/components"
+import { Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
 
 function App() {
   return (
-    <RootLayout>
-      <Sidebar className="p-2 border-4 border-red-500">Sidebar</Sidebar>
-      <Content className="border-4 border-blue-500">Content</Content>
-    </RootLayout>
+    <>
+    <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2 border-4 border-red-500">Sidebar</Sidebar>
+        <Content className="border-4 border-blue-500">Content</Content>
+      </RootLayout>
+    </>
   )
 }
 
