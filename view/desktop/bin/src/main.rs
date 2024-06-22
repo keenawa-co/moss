1f4f8f964b26c73efe2b44960bdcda8212f9cc8f
g10z3r <20g10z3r@gmail.com>
2024-06-22 17:35:33 +0200

    initial rust integration

--- view/desktop/bin/src/main.rs
+++ view/desktop/bin/src/main.rs
@@ -1,16 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::{App, Manager};
+
+mod mem;
+mod menu;
+
+#[macro_use]
+extern crate tracing;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+fn main() -> tauri::Result<()> {
     tauri::Builder::default()
+        .setup(move |app: &mut App| {
+            dbg!(app.webview_windows());
+            Ok(())
+        })
+        .menu(menu::setup_window_menu)
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())?
+        .run(|_, _| {});
+
+    Ok(())
 }
