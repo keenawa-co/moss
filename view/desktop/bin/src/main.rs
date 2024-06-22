d48100e9761a58dc833e0fa4d599f3dcd9f8f1aa
g10z3r <20g10z3r@gmail.com>
2024-06-22 17:59:34 +0200

    Tauri with React

--- view/desktop/bin/src/main.rs
+++ view/desktop/bin/src/main.rs
@@ -1,30 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{App, Manager};
-
-mod mem;
-mod menu;
-
-#[macro_use]
-extern crate tracing;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-fn main() -> tauri::Result<()> {
-    tauri::Builder::default()
-        .setup(move |app: &mut App| {
-            app.get_webview_window("main").unwrap();
-            Ok(())
-        })
-        .menu(menu::setup_window_menu)
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .build(tauri::generate_context!())?
-        .run(|_, _| {});
-
-    Ok(())
+fn main() {
+  app_lib::run();
 }
