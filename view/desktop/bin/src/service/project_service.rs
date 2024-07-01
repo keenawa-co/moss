a5a19423f839023952e012ddf13aa5ce7cf4bb1a
g10z3r <20g10z3r@gmail.com>
2024-07-01 17:53:29 +0200

    Wrote direct queries without using a library function

--- view/desktop/bin/src/service/project_service.rs
+++ view/desktop/bin/src/service/project_service.rs
@@ -1,54 +1,74 @@
 use anyhow::Result;
-use chrono::Utc;
 use specta::Type;
 use std::sync::Arc;
-use surrealdb::{engine::local::Db, Surreal};
+use surrealdb::{engine::remote::ws::Client, sql::Thing, Surreal};
 use types::id::NanoId;
 
 #[derive(Debug, Clone, Type, Deserialize, Serialize)]
 pub struct ProjectMeta {
-    pub id: NanoId,
+    #[specta(type = String)]
+    pub id: Thing,
     pub source: String,
     pub repository: Option<String>,
-    pub opened_at: usize,
-    pub created_at: usize,
+    #[specta(type = String)]
+    pub created_at: i64,
+}
+
+pub struct ProjectService {
+    conn: Arc<Surreal<Client>>,
 }
 
 #[derive(Debug, Clone, Type, Deserialize, Serialize)]
 pub struct CreateProjectMetaInput {
     pub source: String,
-}
-
-pub struct ProjectService {
-    conn: Arc<Surreal<Db>>,
+    pub repository: Option<String>,
 }
 
 impl ProjectService {
-    pub fn new(conn: Arc<Surreal<Db>>) -> Self {
+    pub fn new(conn: Arc<Surreal<Client>>) -> Self {
         Self { conn }
     }
 
-    pub async fn create_project(&self, input: &CreateProjectMetaInput) -> Result<Vec<ProjectMeta>> {
-        let time_now = Utc::now().timestamp() as usize;
-
-        Ok(self
+    pub async fn create_project(
+        &self,
+        input: &CreateProjectMetaInput,
+    ) -> Result<Option<ProjectMeta>> {
+        let surql = "CREATE ONLY type::table($table) CONTENT {
+                source: $input.source,
+                repository: $input.repository,
+                created_at: time::unix(),
+            }";
+        let mut result = self
             .conn
-            .create("project_meta")
-            .content(ProjectMeta {
-                id: NanoId::new(),
-                source: input.source.clone(),
-                repository: None,
-                opened_at: time_now,
-                created_at: time_now,
-            })
-            .await?)
+            .query(surql)
+            .bind(("table", "project_meta"))
+            .bind(("input", input))
+            .await?;
+
+        Ok(result.take(0)?)
     }
 
-    pub async fn get_project_by_id(&self, id: NanoId) -> Result<Option<ProjectMeta>> {
-        Ok(self.conn.select(("project_meta", id.to_string())).await?)
+    pub async fn get_project_by_id(&self, id: String) -> Result<Option<ProjectMeta>> {
+        let surql = "SELECT * FROM ONLY type::thing($table, $id)";
+        let mut result = self
+            .conn
+            .query(surql)
+            .bind(("table", "project_meta"))
+            .bind(("id", id))
+            .await?;
+
+        Ok(result.take(0)?)
     }
 
-    pub async fn delete_project_by_id(&self, id: NanoId) -> Result<Option<ProjectMeta>> {
-        Ok(self.conn.delete(("project_meta", id.to_string())).await?)
+    pub async fn delete_project_by_id(&self, id: String) -> Result<Option<ProjectMeta>> {
+        let surql = "DELETE ONLY type::thing($table, $id)";
+        let mut result = self
+            .conn
+            .query(surql)
+            .bind(("table", "project_meta"))
+            .bind(("id", id))
+            .await?;
+
+        Ok(result.take(0)?)
     }
 }
