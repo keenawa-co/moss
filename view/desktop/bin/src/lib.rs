66dc5c295c12f20cf02355fd93a8a3bdb95febbc
g10z3r <20g10z3r@gmail.com>
2024-07-04 13:41:28 +0200

    feat: Added a template for creating an abstract client (@seniorkonung)

--- view/desktop/bin/src/lib.rs
+++ view/desktop/bin/src/lib.rs
@@ -3,14 +3,39 @@ mod mem;
 pub mod menu;
 pub mod service;
 
+use anyhow::Result;
 use service::project_service::ProjectService;
 use service::session_service::SessionService;
+use surrealdb::{
+    method::Query,
+    opt::{IntoEndpoint, IntoQuery},
+    Connection, Surreal,
+};
 
 #[macro_use]
 extern crate serde;
 
 #[macro_use]
 extern crate tracing;
+
+pub struct SurrealClient<C: Connection>(Surreal<C>);
+
+impl<C: Connection> SurrealClient<C> {
+    pub async fn new<P>(
+        address: impl IntoEndpoint<P, Client = C>,
+        ns: impl Into<String>,
+        db: impl Into<String>,
+    ) -> Result<Self> {
+        let client = Surreal::new::<P>(address).await?;
+        client.use_ns(ns).use_db(db).await?;
+
+        Ok(Self(client))
+    }
+
+    pub fn query(&self, query: impl IntoQuery) -> Query<C> {
+        self.query(query)
+    }
+}
 
 pub struct AppState {
     pub project_service: ProjectService,
