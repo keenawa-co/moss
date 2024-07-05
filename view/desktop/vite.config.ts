a3c5be94d38ab72bcea6b56d421ce54a779fa38a
jevgenis33 <jevgenijs.semjonovs@nagra.com>
2024-07-05 11:19:32 +0200

    Reformating

--- view/desktop/vite.config.ts
+++ view/desktop/vite.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
   assetsInclude: 'src/renderer/assets/**',
   resolve: {
     alias: {
-      "@shared": resolve("shared"),
-      "@/hooks": resolve("src/hooks"),
-      "@/assets": resolve("src/assets"),
-      "@/components": resolve("src/components"),
-      "@/icons": resolve(__dirname, "../shared/components/icons"),
-    },
-  },
-});
+      '@shared': resolve('shared'),
+      '@/hooks': resolve('src/hooks'),
+      '@/assets': resolve('src/assets'),
+      '@/components': resolve('src/components'),
+      '@/icons': resolve(__dirname, '../shared/components/icons')
+    }
+  }
+})
