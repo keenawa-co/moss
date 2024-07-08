a9283ca22a2d035382707c1fad2511b530655f35
jevgenis33 <jevgenijs.semjonovs@nagra.com>
2024-07-08 14:02:48 +0200

    Refactoring.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,10 +2,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 import { dirname, join } from "path";
 
-import path from "path";
-
-import { mergeConfig } from "vite";
-
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
@@ -20,42 +16,15 @@ const config: StorybookConfig = {
     "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   staticDirs: ["../public"],
-  addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
+  addons: [    
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    {
-      name: '@storybook/addon-styling-webpack',
-      options: {
-        rules: [
-          // Replaces existing CSS rules to support PostCSS
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: { importLoaders: 1 }
-              },
-              {
-                // Gets options from `postcss.config.js` in your project root
-                loader: 'postcss-loader',
-                options: { implementation: require.resolve('postcss') }
-              }
-            ],
-          }
-        ]
-      }
-    },
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
-  },
-  viteFinal(config) {
-    return mergeConfig(config, { cacheDir: path.resolve(__dirname, ".cache") });
   },
 };
 export default config;
