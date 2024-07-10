28d5ca0bd6b5ad50939da82b029f6db7bbb7bd38
g10z3r <20g10z3r@gmail.com>
2024-07-10 12:08:26 +0200

    fix: platform-specific code isolation

--- platform/app/src/lib.rs
+++ platform/app/src/lib.rs
@@ -1,12 +1,12 @@
-pub mod context;
+// pub mod context;
 pub mod context_compact;
 pub mod context_model;
 pub mod event;
 
 mod executor;
-mod platform;
+// mod platform;
 
-use context::{AppCell, AppContext};
+// use context::{AppCell, AppContext};
 use context_compact::{AppCellCompact, AppContextCompact};
 use std::{
     cell::{Ref, RefMut},
@@ -16,29 +16,29 @@ use std::{
 #[macro_use]
 extern crate anyhow;
 
-pub struct App(Rc<AppCell>);
+// pub struct App(Rc<AppCell>);
 
-impl App {
-    pub fn new() -> Self {
-        Self(AppContext::new(platform::current_platform()))
-    }
+// impl App {
+//     pub fn new() -> Self {
+//         Self(AppContext::new(platform::current_platform()))
+//     }
 
-    pub fn run<F>(self, on_finish_launching: F)
-    where
-        F: 'static + FnOnce(&AppContext),
-    {
-        let this = self.0.clone();
-        // let platform = self.0.app.borrow().platform.clone();
-        // platform.run(Box::new(move || {
-        //     let ctx: &mut RefMut<AppContext> = &mut *this.borrow_mut();
-        //     on_finish_launching(ctx);
-        // }));
+//     pub fn run<F>(self, on_finish_launching: F)
+//     where
+//         F: 'static + FnOnce(&AppContext),
+//     {
+//         let this = self.0.clone();
+//         // let platform = self.0.app.borrow().platform.clone();
+//         // platform.run(Box::new(move || {
+//         //     let ctx: &mut RefMut<AppContext> = &mut *this.borrow_mut();
+//         //     on_finish_launching(ctx);
+//         // }));
 
-        let ctx: &Ref<AppContext> = &*this.borrow();
-        // let c = ctx.to_owned();
-        on_finish_launching(ctx);
-    }
-}
+//         let ctx: &Ref<AppContext> = &*this.borrow();
+//         // let c = ctx.to_owned();
+//         on_finish_launching(ctx);
+//     }
+// }
 
 pub struct AppCompact(Rc<AppCellCompact>);
 
