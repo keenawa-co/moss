523deaa29b167335cddec39fd5ad3df50791c20e
g10z3r <20g10z3r@gmail.com>
2024-07-18 17:05:16 +0200

    fix(configuration): moved the configuration module to the platform level

--- platform/configuration/src/lib.rs
+++ platform/configuration/src/lib.rs
@@ -1,9 +1,15 @@
-use serde_json::Value;
-
 pub mod configuration_default;
 pub mod configuration_model;
 pub mod configuration_registry;
 pub mod configuration_service;
+
+use serde_json::Value;
+
+#[macro_use]
+extern crate serde;
+
+#[macro_use]
+extern crate lazy_regex;
 
 pub trait AbstractConfigurationService {
     fn get_value(&self, key: &str, overrider_identifier: Option<&str>) -> Option<Value>;
--- view/desktop/bin/src/main.rs
+++ view/desktop/bin/src/main.rs
@@ -16,10 +16,10 @@ use tauri::{App, AppHandle, Manager, State};
 use tauri_specta::{collect_commands, collect_events, ts};
 use tracing::error;
 // use tracing_subscriber::FmtSubscriber;
-use workbench::configuration::configuration_registry::{
+use configuration::configuration_registry::{
     CompositePropertyKey, ConfigurationNodeType, ConfigurationScope, PropertyMap, SourceInfo,
 };
-use workbench::configuration::{
+use configuration::{
     configuration_registry::{
         ConfigurationNode, ConfigurationPropertySchema, ConfigurationRegistry,
     },
