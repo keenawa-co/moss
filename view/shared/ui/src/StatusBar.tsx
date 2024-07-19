99002c32fcba7146a3330c462f28b2936876f8fa
Enthceph <enthceph@gmail.com>
2024-07-19 21:42:14 +0300

    refactor: refactor StatusBar according to new ESLint rule

--- view/shared/ui/src/StatusBar.tsx
+++ view/shared/ui/src/StatusBar.tsx
@@ -1,69 +1,69 @@
 import '../dist/index.css'
-import { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const StatusBar = ({ branch }: { branch?: string }) => {
   return (
-    <div className="ui-flex ui-w-full ui-items-center ui-px-5 ui-bg-olive-900 ui-text-stone-50 ui-text-xs ">
-      <div className="ui-grow">
-        <StatusBarButton className="ui-group">
+    <div className="flex w-full items-center px-5 bg-olive-900 text-stone-50 text-xs ">
+      <div className="grow">
+        <StatusBarButton className="group">
           <svg
-            className="ui-opacity-80 group-hover:ui-opacity-100 group-focus:ui-opacity-100 ui-transition"
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
+            className="opacity-80 group-hover:opacity-100 group-focus:opacity-100 transition"
+            fill="none"
             height="18"
             viewBox="0 0 18 18"
-            fill="none"
+            width="18"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <g stroke="white" opacity=".8">
-              <path strokeLinejoin="round" d="m6 6 2 1.5L6 9" />
-              <path strokeLinecap="round" d="M8.4 12h3" />
-              <rect width="11" height="11" x="3.5" y="3.5" rx="3.5" />
+            <g opacity=".8" stroke="white">
+              <path d="m6 6 2 1.5L6 9" strokeLinejoin="round" />
+              <path d="M8.4 12h3" strokeLinecap="round" />
+              <rect height="11" rx="3.5" width="11" x="3.5" y="3.5" />
             </g>
           </svg>
           <span>Console</span>
         </StatusBarButton>
       </div>
 
-      {branch && (
+      {branch ? (
         <div>
-          <StatusBarButton className="ui-group ui-flex">
+          <StatusBarButton className="group flex">
             <svg
-              className="ui-opacity-80 group-hover:ui-opacity-100 group-focus:ui-opacity-100 ui-transition ui-shrink-0"
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
+              className="opacity-80 group-hover:opacity-100 group-focus:opacity-100 transition shrink-0"
+              fill="none"
               height="18"
               viewBox="0 0 14 15"
-              fill="none"
+              width="18"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <g opacity=".8">
                 <path
-                  fill="#fff"
                   d="M3 13.42a.5.5 0 1 0 1 0H3Zm1 0v-8.4H3v8.4h1Zm-1-8.4V6.2h1V5.02H3ZM3 6.2c0 1.13.4 2.57 1.28 3.73A5.13 5.13 0 0 0 8.5 12v-1c-1.59 0-2.7-.72-3.42-1.68A5.41 5.41 0 0 1 4 6.2H3ZM8.5 12h.78v-1H8.5v1ZM4.23 3.8c0 .4-.33.72-.73.72v1c.95 0 1.73-.77 1.73-1.72h-1Zm-.73.72a.72.72 0 0 1-.72-.72h-1c0 .95.77 1.72 1.72 1.72v-1Zm-.72-.72c0-.4.32-.73.72-.73v-1c-.95 0-1.72.78-1.72 1.73h1Zm.72-.73c.4 0 .73.33.73.73h1c0-.95-.78-1.73-1.73-1.73v1Zm7.73 8.43c0 .4-.33.72-.73.72v1c.95 0 1.73-.77 1.73-1.72h-1Zm-.73.72a.73.73 0 0 1-.72-.72h-1c0 .95.77 1.72 1.72 1.72v-1Zm-.72-.72c0-.4.32-.73.72-.73v-1c-.95 0-1.72.78-1.72 1.73h1Zm.72-.73c.4 0 .73.33.73.73h1c0-.95-.78-1.73-1.73-1.73v1Z"
+                  fill="#fff"
                   opacity=".9"
                 />
               </g>
             </svg>
-            <span className="ui-line-clamp-1">{branch}</span>
+            <span className="line-clamp-1">{branch}</span>
           </StatusBarButton>
         </div>
-      )}
+      ) : null}
 
       <div>
-        <StatusBarButton className="ui-group">
+        <StatusBarButton className="group">
           <svg
-            className="ui-opacity-80 group-hover:ui-opacity-100 group-focus:ui-opacity-100 ui-w-4.5 ui-h-4.5 ui-transition"
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
+            className="opacity-80 group-hover:opacity-100 group-focus:opacity-100 w-4.5 h-4.5 transition"
+            fill="none"
             height="18"
             viewBox="0 0 14 14"
-            fill="none"
+            width="18"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
+              clipRule="evenodd"
+              d="M5.36 1.48a.53.53 0 0 0-.56.89c.26.17.48.41.69.7a.52.52 0 1 0 .85-.6 3.57 3.57 0 0 0-.98-.99Zm1.52 2.95-2.25.22.09.37.25.73.34.67.39.61.03.04.03-.05.43-.74.35-.79.28-.85.06-.2Zm1.11-.1 1.34-.13a.52.52 0 1 0-.1-1.05l-7.35.7a.52.52 0 1 0 .1 1.05l1.6-.15.13.57.3.84.38.78.46.71.23.28-.37.44-.64.61-.71.56-.8.53-.89.5a.52.52 0 1 0 .51.91l.92-.51.87-.58.79-.62.71-.68.34-.4.2.19.66.5.7.42-.22.46-.01.02-.78 1.56a.52.52 0 1 0 .94.47l.64-1.28h3.02l.65 1.28a.53.53 0 0 0 .94-.47l-.79-1.56v-.02L9.91 6.59a.52.52 0 0 0-.94 0L7.85 8.86l-.59-.35-.58-.44-.22-.2.2-.29.47-.82.4-.89.3-.93.16-.61Zm2.45 5.65H8.46l1-1.98.98 1.98Z"
               fill="#fff"
               fillRule="evenodd"
-              d="M5.36 1.48a.53.53 0 0 0-.56.89c.26.17.48.41.69.7a.52.52 0 1 0 .85-.6 3.57 3.57 0 0 0-.98-.99Zm1.52 2.95-2.25.22.09.37.25.73.34.67.39.61.03.04.03-.05.43-.74.35-.79.28-.85.06-.2Zm1.11-.1 1.34-.13a.52.52 0 1 0-.1-1.05l-7.35.7a.52.52 0 1 0 .1 1.05l1.6-.15.13.57.3.84.38.78.46.71.23.28-.37.44-.64.61-.71.56-.8.53-.89.5a.52.52 0 1 0 .51.91l.92-.51.87-.58.79-.62.71-.68.34-.4.2.19.66.5.7.42-.22.46-.01.02-.78 1.56a.52.52 0 1 0 .94.47l.64-1.28h3.02l.65 1.28a.53.53 0 0 0 .94-.47l-.79-1.56v-.02L9.91 6.59a.52.52 0 0 0-.94 0L7.85 8.86l-.59-.35-.58-.44-.22-.2.2-.29.47-.82.4-.89.3-.93.16-.61Zm2.45 5.65H8.46l1-1.98.98 1.98Z"
-              clipRule="evenodd"
               opacity=".8"
             />
           </svg>
@@ -72,20 +72,20 @@ const StatusBar = ({ branch }: { branch?: string }) => {
       </div>
 
       <div>
-        <StatusBarButton className="ui-group">
+        <StatusBarButton className="group">
           <svg
-            className="ui-opacity-80 group-hover:ui-opacity-100 group-focus:ui-opacity-100 ui-w-4.5 ui-h-4.5 ui-transition"
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
+            className="opacity-80 group-hover:opacity-100 group-focus:opacity-100 w-4.5 h-4.5 transition"
+            fill="none"
             height="18"
             viewBox="-4 -4 18 18"
-            fill="none"
+            width="18"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
+              clipRule="evenodd"
+              d="M6 1.1a4.9 4.9 0 1 0 0 9.8 4.9 4.9 0 0 0 0-9.8ZM0 6a6 6 0 1 1 12 0A6 6 0 0 1 0 6Zm5.3-2.3a.7.7 0 1 1 1.4 0 .7.7 0 0 1-1.4 0ZM6 5.5c.3 0 .5.2.5.5v2.5a.5.5 0 1 1-1 0V6c0-.3.2-.5.5-.5Z"
               fill="#fff"
               fillRule="evenodd"
-              d="M6 1.1a4.9 4.9 0 1 0 0 9.8 4.9 4.9 0 0 0 0-9.8ZM0 6a6 6 0 1 1 12 0A6 6 0 0 1 0 6Zm5.3-2.3a.7.7 0 1 1 1.4 0 .7.7 0 0 1-1.4 0ZM6 5.5c.3 0 .5.2.5.5v2.5a.5.5 0 1 1-1 0V6c0-.3.2-.5.5-.5Z"
-              clipRule="evenodd"
             />
           </svg>
           <span>About</span>
@@ -97,13 +97,15 @@ const StatusBar = ({ branch }: { branch?: string }) => {
 
 export default StatusBar
 
-const StatusBarButton = ({ children, className }: ComponentPropsWithoutRef<'button'>) => (
-  <button
-    className={twMerge(
-      'ui-flex  ui-items-center ui-gap-2 ui-grow hover:ui-bg-white hover:ui-bg-opacity-10 focus:ui-bg-white focus:ui-bg-opacity-10 ui-transition ui-px-2.5 ui-py-0.5',
-      className
-    )}
-  >
-    {children}
-  </button>
-)
+function StatusBarButton({ children, className }: ComponentPropsWithoutRef<'button'>) {
+  return (
+    <button
+      className={twMerge(
+        'flex  items-center gap-2 grow hover:bg-white hover:bg-opacity-10 focus:bg-white focus:bg-opacity-10 transition px-2.5 py-0.5',
+        className
+      )}
+    >
+      {children}
+    </button>
+  )
+}
