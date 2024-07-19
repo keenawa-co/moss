63c86511224a47ed56ccb46e318e2d65557829f0
Enthceph <enthceph@gmail.com>
2024-07-19 09:07:26 +0300

    feat: added simple statusbar

--- view/shared/config-tailwind/tailwind.config.ts
+++ view/shared/config-tailwind/tailwind.config.ts
@@ -1,15 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
+const config: Omit<Config, 'content'> = {
   theme: {
     extend: {
       backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
-      },
+        'glow-conic': 'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)'
+      }
     },
+    colors: {
+      olive: {
+        50: '#FAF9E0',
+        100: '#F5F2C2',
+        200: '#EBE586',
+        300: '#E0D849',
+        400: '#D7CF1A',
+        500: '#C0B60B',
+        600: '#A39B09',
+        700: '#807707',
+        800: '#5D5405',
+        900: '#3A3203',
+        950: '#1A1A00'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
---
+++ view/shared/ui/src/StatusBar.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import StatusBar from './StatusBar'
+import '../dist/index.css'
+
+const meta = {
+  title: 'StatusBar',
+  component: StatusBar,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {}
+} satisfies Meta<typeof StatusBar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const WithBranch: Story = {
+  args: {
+    branch: 'MOSSMVP-37-Backend-Migrate-existing-backend-in-Tauri'
+  }
+}
+
+export const NoBranch: Story = {}
