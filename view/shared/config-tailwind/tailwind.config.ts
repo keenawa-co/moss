dc62dec133e49f4db23215061dc92063cabf221e
Enthceph <enthceph@gmail.com>
2024-07-20 11:37:48 +0300

    feat: added olive colors to shared tailwind config

--- view/shared/config-tailwind/tailwind.config.ts
+++ view/shared/config-tailwind/tailwind.config.ts
@@ -6,21 +6,21 @@ const config: Omit<Config, 'content'> = {
     extend: {
       backgroundImage: {
         'glow-conic': 'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)'
-      }
-    },
-    colors: {
-      olive: {
-        50: '#FAF9E0',
-        100: '#F5F2C2',
-        200: '#EBE586',
-        300: '#E0D849',
-        400: '#D7CF1A',
-        500: '#C0B60B',
-        600: '#A39B09',
-        700: '#807707',
-        800: '#5D5405',
-        900: '#3A3203',
-        950: '#1A1A00'
+      },
+      colors: {
+        olive: {
+          50: '#FAF9E0',
+          100: '#F5F2C2',
+          200: '#EBE586',
+          300: '#E0D849',
+          400: '#D7CF1A',
+          500: '#C0B60B',
+          600: '#A39B09',
+          700: '#807707',
+          800: '#5D5405',
+          900: '#3A3203',
+          950: '#1A1A00'
+        }
       }
     }
   },
