12c742899874b1db5d44b24c03a20c92318a086a
Enthceph <enthceph@gmail.com>
2024-07-22 17:53:02 +0300

    feat: updated tailwind and storybook configs

--- view/storybook/tailwind.config.ts
+++ view/storybook/tailwind.config.ts
@@ -1,10 +1,13 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from 'tailwindcss'
+import sharedConfig from '@repo/tailwind-config'
 
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["../shared/ui/src/**/*.{js,ts,jsx,tsx,mdx}"],
-  //prefix: "ui-",
+const config: Pick<Config, 'presets' | 'content'> = {
   presets: [sharedConfig],
-};
+  content: [
+    '../shared/ui/src/**/*.{js,ts,jsx,tsx,mdx}',
+    '../desktop/src/**/*.{js,jsx,mjs,ts,tsx}',
+    '../web/src/**/*.{js,jsx,mjs,ts,tsx}'
+  ]
+}
 
-export default config;
+export default config
