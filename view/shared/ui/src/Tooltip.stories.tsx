00c2ffcd67f2abb61665cfcc586a9636da498e3b
Enthceph <enthceph@gmail.com>
2024-08-06 16:28:39 +0300

    feat: added Tooltip

---
+++ view/shared/ui/src/Tooltip.stories.tsx
@@ -0,0 +1,116 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Tooltip from "./Tooltip";
+import "./styles.css";
+
+const meta = {
+  title: "Shared/TooltipDemo",
+  component: Tooltip,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Tooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const TooltipTemplate = (args: any) => (
+  <Tooltip {...args}>
+    <button>Tooltip</button>
+  </Tooltip>
+);
+
+export const WithShortcut: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const NoShortcut: Story = {
+  args: {
+    label: "Settings",
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const ALotOfText: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: 'Not a lorem ipsum text, because spell checker marks words from lorem ipsum text as "problems"',
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const WithArrow: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+    options: {
+      arrow: {},
+    },
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const SideBottom: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+    options: {
+      content: {
+        side: "bottom",
+      },
+    },
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const AlignStart: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+    options: {
+      content: {
+        align: "start",
+      },
+    },
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const InstantOpen: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+    options: {
+      provider: {
+        delayDuration: 0,
+      },
+    },
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const AlwaysOpen: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: "Settings",
+    options: {
+      root: {
+        open: true,
+      },
+    },
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
+
+export const CustomClassName: Story = {
+  args: {
+    shortcut: ["⌘", "s"],
+    label: 'Not a lorem ipsum text, because spell checker marks words from lorem ipsum text as "problems"',
+    className: "bg-red-500 text-blue-600 p-6 max-w-full text-2xl font-bold italic",
+  },
+  render: (args) => <TooltipTemplate {...args} />,
+};
---
+++ view/shared/ui/src/Tooltip.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { cn } from "./lib/utils";
+
+export interface TooltipOptions {
+  provider?: Pick<
+    TooltipPrimitive.TooltipProviderProps,
+    "delayDuration" | "skipDelayDuration" | "disableHoverableContent"
+  >;
+  root?: Pick<
+    TooltipPrimitive.TooltipProps,
+    "defaultOpen" | "open" | "onOpenChange" | "delayDuration" | "disableHoverableContent"
+  >;
+  content?: Pick<
+    TooltipPrimitive.TooltipContentProps,
+    | "aria-label"
+    | "onEscapeKeyDown"
+    | "onPointerDownOutside"
+    | "forceMount"
+    | "side"
+    | "sideOffset"
+    | "align"
+    | "alignOffset"
+    | "avoidCollisions"
+    | "collisionBoundary"
+    | "collisionPadding"
+    | "arrowPadding"
+    | "sticky"
+    | "hideWhenDetached"
+  >;
+  arrow?: Pick<TooltipPrimitive.TooltipArrowProps, "asChild" | "width" | "height">;
+  portal?: Pick<TooltipPrimitive.TooltipPortalProps, "forceMount" | "container">;
+}
+const Tooltip = ({
+  label,
+  shortcut, //TODO shortcut doesn't have any functionality
+  options,
+  children,
+  className,
+}: {
+  label?: string;
+  shortcut?: string[];
+  options?: TooltipOptions;
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <TooltipPrimitive.Provider {...options?.provider}>
+      <TooltipPrimitive.Root {...options?.root}>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Content
+          className={cn(
+            `z-50 overflow-hidden border-none flex gap-2.5 bg-black text-white py-1 px-2 rounded-md shadow-md text-xs leading-3 max-w-44
+              data-[state=closed]:animate-out 
+              data-[state=closed]:fade-out-0 
+              data-[state=closed]:zoom-out-95 
+              data-[side=bottom]:slide-in-from-top-2 
+              data-[side=left]:slide-in-from-right-2 
+              data-[side=right]:slide-in-from-left-2 
+              data-[side=top]:slide-in-from-bottom-2`,
+            className
+          )}
+          {...options?.content}
+        >
+          {options?.portal && <TooltipPrimitive.Portal {...options?.portal} />}
+          {options?.arrow && <TooltipPrimitive.Arrow {...options?.arrow} />}
+          <div>{label}</div>
+          {shortcut && (
+            <div className="text-neutral-400 uppercase self-center">
+              {shortcut.map((s) => (
+                <span key={s}>{s}</span>
+              ))}
+            </div>
+          )}
+        </TooltipPrimitive.Content>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  );
+};
+
+export default Tooltip;
