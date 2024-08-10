724d5c837c2ba34a7bafa5f30477319cb6d4af53
EnthusedFear <enthceph@gmail.com>
2024-08-10 20:31:53 +0300

    feat: changed styles for icons

--- view/shared/ui/src/CodeIcon.tsx
+++ view/shared/ui/src/CodeIcon.tsx
@@ -8,6 +8,7 @@ export const CodeIcon = ({ className }: CodeIconProps) => {
   return (
     <svg
       className={twMerge("flex items-center fill-current", className)}
+      viewBox="0 0 16 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
