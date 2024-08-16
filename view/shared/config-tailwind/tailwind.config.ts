49b1d2d728b76ff4cc3c1212b3d085486ff7419e
Enthceph <enthceph@gmail.com>
2024-08-16 10:56:13 +0300

    feat: added font family for shortcut

--- view/shared/config-tailwind/tailwind.config.ts
+++ view/shared/config-tailwind/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Omit<Config, "content"> = {
     extend: {
       fontFamily: {
         sans: ["Lato", "Inter"],
+        shortcut: ["Inter"],
       },
       backgroundImage: {
         "glow-conic": "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
