984da7d3aad0693e16e5439d4d5a321b68aa6ed1
Enthceph <enthceph@gmail.com>
2024-08-16 20:34:42 +0300

    feat: added All Variants story to Icon stories

--- view/shared/ui/src/Icon.stories.tsx
+++ view/shared/ui/src/Icon.stories.tsx
@@ -22,6 +22,31 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const AllVariants: Story = {
+  args: {
+    icon: "Home1",
+    className: "text-6xl",
+  },
+  render: (args) => {
+    return (
+      <div className="w-full p-16 flex flex-col gap-6">
+        <h2 className="text-3xl font-semibold">All variants</h2>
+
+        <div className="grid grid-cols-4 gap-4">
+          <div>Default</div>
+          <div>Fill</div>
+          <div>Stroke</div>
+          <div>Without default color</div>
+          <Icon icon={args.icon} className="text-6xl" />
+          <Icon icon={args.icon} className="text-6xl text-green-400" />
+          <Icon icon={args.icon} className="text-6xl stroke-1 stroke-green-400" viewBox="-1 -1 18 17" />
+          <Icon icon="NewProject" className="text-6xl" />
+        </div>
+      </div>
+    );
+  },
+};
+
 export const Default: Story = {
   args: {
     icon: "Home1",
