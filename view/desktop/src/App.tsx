7baea801658da4976fe2690093b1677427b4eb2c
Enthceph <enthceph@gmail.com>
2024-08-20 23:16:14 +0300

    feat: added base of Resizable component

--- view/desktop/src/App.tsx
+++ view/desktop/src/App.tsx
@@ -1,11 +1,13 @@
 import { About, Content, TitleBar, Home, Menu, RootLayout, Sidebar } from "@/components";
-import { Suspense } from "react";
+import { Suspense, useRef, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "@/i18n";
 import "@repo/ui/src/fonts.css";
 import { Icon, MenuItem, IconTitle } from "@repo/ui";
 import { twMerge } from "tailwind-merge";
 import StatusBar from "@/components/StatusBar";
+import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "./components/Resizable";
+import { ImperativePanelGroupHandle } from "react-resizable-panels";
 
 enum IconState {
   Default = "group-text-zinc-500",
@@ -17,69 +19,94 @@ enum IconState {
 }
 
 function App() {
+  const myRef = useRef<ImperativePanelGroupHandle>(null);
   return (
     <>
       <RootLayout>
         <TitleBar />
-        <Sidebar className="p-0">
-          <MenuItem className="group bg-zinc-200 mt-13 mb-3.5">
-            <Icon icon="Search" className={twMerge("h-4.5 w-4.5", IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-xs bg-zin" title="Search..." />
-            <Icon icon="SearchShortcut" className="fill-zinc-500  group-hover:fill-zinc-600 text-3xl ml-auto pr-2" />
-          </MenuItem>
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="RootLayout"
+          onLayout={(sizes: number[]) => {
+            console.log("sizes", sizes);
+          }}
+          ref={myRef}
+        >
+          <ResizablePanel maxSize={40} minSize={5} collapsible defaultSize={14.5}>
+            <Sidebar className="p-0 h-full ">
+              <MenuItem className="group bg-zinc-200 mt-13 mb-3.5 overflow-hidden">
+                <Icon icon="Search" className={twMerge("h-4.5 w-4.5", IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-xs bg-zin" title="Search..." />
+                <Icon
+                  icon="SearchShortcut"
+                  className="fill-zinc-500  group-hover:fill-zinc-600 text-3xl ml-auto pr-2"
+                />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Home" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Home" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Issues" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Issues" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Code" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Code" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Goals" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Goals" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Reports" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Reports" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Documentation" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Documentation" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Settings" />
-          </MenuItem>
+              <MenuItem className="group">
+                <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Settings" />
+              </MenuItem>
 
-          <MenuItem className="group">
-            <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover)} />
-            <IconTitle className="text-zinc-900 text-sm" title="Quick Search" />
-          </MenuItem>
-        </Sidebar>
+              <MenuItem className="group">
+                <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover)} />
+                <IconTitle className="text-zinc-900 text-sm" title="Quick Search" />
+              </MenuItem>
+            </Sidebar>
+          </ResizablePanel>
 
-        <Content className="relative flex flex-col">
-          <Suspense fallback="loading">
-            <BrowserRouter>
-              <Menu />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-              </Routes>
-            </BrowserRouter>
-          </Suspense>
-        </Content>
+          <ResizableHandle
+            onDoubleClick={() => {
+              console.log(myRef.current);
+              myRef.current?.setLayout([14.5, 100 - 14.5]);
+            }}
+          />
+
+          <ResizablePanel>
+            <Content className="flex flex-col">
+              <Suspense fallback="loading">
+                <BrowserRouter>
+                  <Menu />
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                  </Routes>
+                </BrowserRouter>
+              </Suspense>
+            </Content>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+
         <StatusBar
           className="absolute w-full bottom-0 h-5.5"
           branch="MOSSMVP-37-Backend-Migrate-existing-backend-in-Tauri"
