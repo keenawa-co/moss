7d06d8847a6581227990c03599035ce4933cb0a6
Enthceph <enthceph@gmail.com>
2024-08-20 23:15:09 +0300

    fix: cannot find window context

--- view/desktop/src/utils/index.ts
+++ view/desktop/src/utils/index.ts
@@ -1,14 +1,4 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
-  dateStyle: "short",
-  timeStyle: "short",
-  timeZone: "UTC",
-});
-
-export const formatDateFromMs = (ms: number) => dateFormatter.format(ms);
-
-export const cn = (...args: ClassValue[]) => {
-  return twMerge(clsx(...args));
-};
+export const cn = (...args: ClassValue[]) => twMerge(clsx(...args));
