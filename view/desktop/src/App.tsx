384cd3dd87ef5242d0f7e39dcef67be2df95498b
Enthceph <enthceph@gmail.com>
2024-08-28 13:37:37 +0300

    feat: added new Resizable library

--- view/desktop/src/App.tsx
+++ view/desktop/src/App.tsx
@@ -6,8 +6,7 @@ import "@repo/ui/src/fonts.css";
 import { Icon, MenuItem, IconTitle } from "@repo/ui";
 import { twMerge } from "tailwind-merge";
 import StatusBar from "@/components/StatusBar";
-import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "./components/ResizablePanel";
-import { ImperativePanelGroupHandle } from "react-resizable-panels";
+import { Resizable, ResizablePanel } from "./components/Resizable";
 
 enum IconState {
   Default = "group-text-zinc-500",
@@ -19,101 +18,82 @@ enum IconState {
 }
 
 function App() {
-  const myRef = useRef<ImperativePanelGroupHandle>(null);
+  const [sideBarVisible, setSideBarVisibility] = useState(true);
 
   return (
-    <>
-      <RootLayout>
-        <TitleBar />
+    <RootLayout>
+      <TitleBar />
 
-        <ResizablePanelGroup
-          direction="horizontal"
-          autoSaveId="RootLayout"
-          onLayout={(sizes: number[]) => {
-            console.log("sizes", sizes);
-          }}
-          ref={myRef}
-        >
-          <ResizablePanel maxSize={40} minSize={5} collapsible defaultSize={14.5}>
-            <Sidebar className="p-0 h-full ">
-              <MenuItem className="group bg-zinc-200 mt-13 mb-3.5 overflow-hidden">
-                <Icon icon="Search" className={twMerge("h-4.5 w-4.5", IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-xs bg-zin" title="Search..." />
-                <Icon
-                  icon="SearchShortcut"
-                  className="fill-zinc-500  group-hover:fill-zinc-600 text-3xl ml-auto pr-2"
-                />
-              </MenuItem>
+      <Resizable proportionalLayout={false}>
+        <ResizablePanel minSize={255} preferredSize={255} snap visible={sideBarVisible}>
+          <Sidebar className="p-0 h-full ">
+            <MenuItem className="group bg-zinc-200 mt-13 mb-3.5 overflow-hidden">
+              <Icon icon="Search" className={twMerge("h-4.5 w-4.5", IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-xs bg-zin" title="Search..." />
+              <Icon icon="SearchShortcut" className="fill-zinc-500  group-hover:fill-zinc-600 text-3xl ml-auto pr-2" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Home" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Home" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Issues" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Issues" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Code" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Code" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Goals" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Goals" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Reports" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Reports" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Documentation" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Documentation" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Settings" />
-              </MenuItem>
+            <MenuItem className="group">
+              <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Settings" />
+            </MenuItem>
 
-              <MenuItem className="group">
-                <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover)} />
-                <IconTitle className="text-zinc-900 text-sm" title="Quick Search" />
-              </MenuItem>
-            </Sidebar>
-          </ResizablePanel>
+            <MenuItem className="group">
+              <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover)} />
+              <IconTitle className="text-zinc-900 text-sm" title="Quick Search" />
+            </MenuItem>
+          </Sidebar>
+        </ResizablePanel>
+        <ResizablePanel>
+          <Content className="flex flex-col">
+            <Suspense fallback="loading">
+              <BrowserRouter>
+                <Menu />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                </Routes>
+              </BrowserRouter>
+            </Suspense>
+          </Content>
+        </ResizablePanel>
+      </Resizable>
 
-          <ResizableHandle
-            onDoubleClick={() => {
-              myRef.current?.setLayout([14.5, 100 - 14.5]);
-            }}
-          />
-
-          <ResizablePanel>
-            <Content className="flex flex-col">
-              <Suspense fallback="loading">
-                <BrowserRouter>
-                  <Menu />
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                  </Routes>
-                </BrowserRouter>
-              </Suspense>
-            </Content>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-
-        <StatusBar
-          className="absolute w-full bottom-0 h-5.5"
-          branch="MOSSMVP-37-Backend-Migrate-existing-backend-in-Tauri"
-        />
-      </RootLayout>
-    </>
+      <StatusBar
+        className="absolute w-full bottom-0 h-5.5"
+        branch="MOSSMVP-37-Backend-Migrate-existing-backend-in-Tauri"
+      />
+    </RootLayout>
   );
 }
 
---
+++ view/desktop/src/components/Resizable.stories.tsx
@@ -0,0 +1,155 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { render } from "react-dom";
+import { Resizable, ResizablePanel } from "./Resizable";
+import "allotment/dist/style.css";
+import { useState } from "react";
+import { cn } from "@/shared/lib/utils";
+
+const meta = {
+  title: "desktop/Resizable",
+  component: Resizable,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <div className="h-96 border border-dashed">
+          <Story />
+        </div>
+      );
+    },
+  ],
+} satisfies Meta<typeof Resizable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <Resizable>
+        <ResizablePanel className="grid place-items-center">One</ResizablePanel>
+        <ResizablePanel className="grid place-items-center">Two</ResizablePanel>
+      </Resizable>
+    );
+  },
+};
+
+export const Vertical: Story = {
+  render: () => {
+    return (
+      <Resizable vertical>
+        <ResizablePanel className="grid place-items-center">One</ResizablePanel>
+        <ResizablePanel className="grid place-items-center">Two</ResizablePanel>
+      </Resizable>
+    );
+  },
+};
+
+export const CollapsingPanel: Story = {
+  render: () => {
+    return (
+      <Resizable>
+        <ResizablePanel className="grid place-items-center" minSize={200} snap>
+          Collapsible
+        </ResizablePanel>
+        <ResizablePanel className="grid place-items-center">Two</ResizablePanel>
+      </Resizable>
+    );
+  },
+};
+
+export const MinMax: Story = {
+  render: () => {
+    return (
+      <Resizable>
+        <ResizablePanel minSize={100} maxSize={600} className="grid place-items-center">
+          <span className="text-center">
+            MinMax: <b>100 - 600px</b>
+          </span>
+        </ResizablePanel>
+        <ResizablePanel className="grid place-items-center">Two</ResizablePanel>
+      </Resizable>
+    );
+  },
+};
+
+export const Nested: Story = {
+  render: () => {
+    return (
+      <Resizable vertical>
+        <ResizablePanel className="grid place-items-center">
+          <Resizable>
+            <ResizablePanel className="grid place-items-center">One</ResizablePanel>
+            <ResizablePanel className="grid place-items-center">Two</ResizablePanel>
+            <ResizablePanel className="grid place-items-center">Three</ResizablePanel>
+          </Resizable>
+        </ResizablePanel>
+        <ResizablePanel className="grid place-items-center">Four</ResizablePanel>
+      </Resizable>
+    );
+  },
+};
+
+export const Controls: Story = {
+  render: () => {
+    const [oneVisible, setOneVisible] = useState(true);
+    const [twoVisible, setTwoVisible] = useState(true);
+    const [threeVisible, setThreeVisible] = useState(true);
+    const [fourVisible, setFourVisible] = useState(true);
+
+    return (
+      <>
+        <div className="flex gap-2 p-4 ">
+          <button
+            className={cn(`border rounded p-2 duration-0`, { "bg-sky-500 border-sky-500 text-white": oneVisible })}
+            onClick={() => setOneVisible(!oneVisible)}
+          >
+            One
+          </button>
+          <button
+            className={cn(`border rounded p-2 duration-0`, { "bg-sky-500 border-sky-500 text-white": twoVisible })}
+            onClick={() => setTwoVisible(!twoVisible)}
+          >
+            Two
+          </button>
+          <button
+            className={cn(`border rounded p-2 duration-0`, { "bg-sky-500 border-sky-500 text-white": threeVisible })}
+            onClick={() => setThreeVisible(!threeVisible)}
+          >
+            Three
+          </button>
+          <button
+            className={cn(`border rounded p-2 duration-0`, { "bg-sky-500 border-sky-500 text-white": fourVisible })}
+            onClick={() => setFourVisible(!fourVisible)}
+          >
+            Four
+          </button>
+        </div>
+        <div className="h-80 pb-2">
+          <Resizable vertical>
+            <ResizablePanel className="grid place-items-center">
+              <Resizable>
+                <ResizablePanel className="grid place-items-center" visible={oneVisible}>
+                  One
+                </ResizablePanel>
+                <ResizablePanel className="grid place-items-center" visible={twoVisible}>
+                  Two
+                </ResizablePanel>
+                <ResizablePanel className="grid place-items-center" visible={threeVisible}>
+                  Three
+                </ResizablePanel>
+              </Resizable>
+            </ResizablePanel>
+            <ResizablePanel className="grid place-items-center" visible={fourVisible}>
+              Four
+            </ResizablePanel>
+          </Resizable>
+        </div>
+      </>
+    );
+  },
+};
