53aecee96d9ce456937562210e6e15d9bb4e1fb6
Enthceph <enthceph@gmail.com>
2024-08-29 21:11:14 +0300

    feat: added text ellipsis and content overflow for Resizable component on desktop main page

--- view/desktop/src/App.tsx
+++ view/desktop/src/App.tsx
@@ -51,51 +51,54 @@ function App() {
         <TitleBar />
 
         <Resizable proportionalLayout={false}>
-          <ResizablePanel minSize={150} preferredSize={255} snap visible={sideBarVisible}>
-            <Sidebar className="p-0 h-full ">
-              <MenuItem className="group bg-zinc-200 mt-13 mb-3.5 overflow-hidden">
-                <Icon icon="Search" className={twMerge("h-4.5 w-4.5", IconState.Default, IconState.Hover)} />
+          <ResizablePanel minSize={100} preferredSize={255} snap visible={sideBarVisible}>
+            <Sidebar className="p-0 h-full w-full">
+              <MenuItem className="group bg-zinc-200 mt-13 mb-3.5">
+                <Icon icon="Search" className={twMerge("h-4.5 w-4.5 min-w-4", IconState.Default, IconState.Hover)} />
                 <IconTitle className="text-primary text-xs" title="Search..." />
-                <Icon icon="SearchShortcut" className="fill-zinc-500 group-hover:fill-zinc-600 text-3xl ml-auto pr-2" />
+                {/* <Icon
+                  icon="SearchShortcut"
+                  className="min-w-4  w-4.5 fill-zinc-500 group-hover:fill-zinc-600  ml-auto pr-2"
+                /> */}
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Home1" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Home" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Issues" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Issues" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Code" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Code" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Goals" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Goals" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Reports" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Reports" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Documentation" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Documentation" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="Settings" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Settings" />
               </MenuItem>
 
               <MenuItem className="group">
-                <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover)} />
+                <Icon icon="QuickSearch" className={twMerge(IconState.Default, IconState.Hover, "min-w-4")} />
                 <IconTitle className="text-primary text-sm" title="Quick Search" />
               </MenuItem>
             </Sidebar>
