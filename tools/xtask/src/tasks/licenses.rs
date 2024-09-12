43df50c52a2b880fc5dc34782a419bd7828b0289
jevgenis33 <jevgenijs.semjonovs@nagra.com>
2024-09-12 22:44:04 +0200

    refactor: refactoring of license

--- tools/xtask/src/tasks/licenses.rs
+++ tools/xtask/src/tasks/licenses.rs
@@ -24,61 +24,65 @@ pub fn run_licenses(_args: LicensesArgs) -> Result<()> {
     let default_license = workspace.workspace_root.join(LICENSE_FILES[0]);
 
     for package in workspace.workspace_packages() {
-        let crate_dir = package
+        let crate_path = package
             .manifest_path
             .parent()
             .ok_or_else(|| anyhow!("no crate directory for '{}'", package.name))?;
 
-        if let Some((license_file, license_index)) = first_license_file(crate_dir, LICENSE_FILES) {
-            let root_license = pathdiff::diff_paths(
+        if let Some((symlink_path, license_index)) =
+            get_first_license_symlink_path(crate_path, LICENSE_FILES)
+        {
+            let root_license_path = pathdiff::diff_paths(
                 workspace.workspace_root.join(LICENSE_FILES[license_index]),
-                crate_dir,
+                crate_path,
             )
             .expect("failed to create relative path for root license");
-            if license_file.is_symlink() {
-                let target = fs::read_link(&license_file)?;
-                if target != root_license {
+            if symlink_path.is_symlink() {
+                let target = fs::read_link(&symlink_path)?;
+                if target != root_license_path {
                     info!("updating symlink for '{}'", package.name);
-                    fs::remove_file(&license_file)?;
-                    create_symlink(&root_license, &license_file)?;
+                    fs::remove_file(&symlink_path)?;
+                    create_symlink(&root_license_path, &symlink_path)?;
                 }
             } else {
                 info!("replacing file with symlink for '{}'", package.name);
-                fs::remove_file(&license_file)?;
-                create_symlink(&root_license, &license_file)?;
+                fs::remove_file(&symlink_path)?;
+                create_symlink(&root_license_path, &symlink_path)?;
             }
         } else {
             info!("creating license symlink for '{}'", package.name);
-            let license_new_path = crate_dir.join(LICENSE_FILES[0]);
-            let relative_default_license = pathdiff::diff_paths(&default_license, crate_dir)
+            let new_symlink_path = crate_path.join(LICENSE_FILES[0]);
+            let default_license_path = pathdiff::diff_paths(&default_license, crate_path)
                 .ok_or_else(|| anyhow!("failed to create relative path for default license"))?;
-            create_symlink(&relative_default_license, &license_new_path.as_std_path())?;
+            create_symlink(&default_license_path, &new_symlink_path.as_std_path())?;
         }
     }
 
     Ok(())
 }
 
-fn first_license_file(path: impl AsRef<Path>, license_files: &[&str]) -> Option<(PathBuf, usize)> {
+fn get_first_license_symlink_path(
+    crate_path: impl AsRef<Path>,
+    license_files: &[&str],
+) -> Option<(PathBuf, usize)> {
     for (index, license_file) in license_files.iter().enumerate() {
-        let path_to_license = path.as_ref().join(license_file);
+        let path_to_license = crate_path.as_ref().join(license_file);
         info!("analyzing '{}'...", path_to_license.display());
-        if std::fs::read_link(&path_to_license).is_ok() {
+        if path_to_license.exists() || std::fs::read_link(&path_to_license).is_ok() {
             return Some((path_to_license, index));
         }
     }
-
     None
 }
 
 #[cfg(unix)]
-fn create_symlink(src: &Path, dst: &Path) -> Result<()> {
-    std::os::unix::fs::symlink(src, dst)?;
+fn create_symlink(license_path: &Path, symlink_path: &Path) -> Result<()> {
+    std::os::unix::fs::symlink(license_path, symlink_path)?;
     Ok(())
 }
 
 #[cfg(windows)]
-fn create_symlink(src: &Path, dst: &Path) -> Result<()> {
-    std::os::windows::fs::symlink_file(src, dst)?;
+fn create_symlink(license_path: &Path, symlink_path: &Path) -> Result<()> {
+    std::os::windows::fs::symlink_file(license_path, symlink_path)?;
     Ok(())
 }
