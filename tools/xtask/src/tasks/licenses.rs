fe2aa74837093b3d6f97181d68546dcd0007e8c2
jevgenis33 <jevgenijs.semjonovs@nagra.com>
2024-09-12 20:23:00 +0200

    refactor: add logging + symlinks replaced witt relative path

--- tools/xtask/src/tasks/licenses.rs
+++ tools/xtask/src/tasks/licenses.rs
@@ -4,12 +4,20 @@ use std::path::{Path, PathBuf};
 use anyhow::{anyhow, Result};
 use clap::Parser;
 
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
 use crate::workspace::load_workspace;
 
 #[derive(Parser)]
 pub struct LicensesArgs {}
 
 pub fn run_licenses(_args: LicensesArgs) -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     const LICENSE_FILES: &[&str] = &["LICENSE-MIT"];
 
     let workspace = load_workspace()?;
@@ -19,25 +27,37 @@ pub fn run_licenses(_args: LicensesArgs) -> Result<()> {
         let crate_dir = package
             .manifest_path
             .parent()
-            .ok_or_else(|| anyhow!("no crate directory for {}", package.name))?;
+            .ok_or_else(|| anyhow!("no crate directory for '{}'", package.name))?;
 
         if let Some(license_file) = first_license_file(crate_dir, LICENSE_FILES) {
             if license_file.is_symlink() {
                 let target = fs::read_link(&license_file)?;
                 if target != root_license {
-                    println!("updating symlink for {}", package.name);
+                    info!("updating symlink for '{}'", package.name);
                     fs::remove_file(&license_file)?;
-                    create_symlink(&root_license.as_std_path(), &license_file)?;
+                    create_relative_symlink(
+                        &root_license.as_std_path(),
+                        &license_file,
+                        &workspace.workspace_root.as_std_path(),
+                    )?;
                 }
             } else {
-                println!("replacing file with symlink for {}", package.name);
+                info!("replacing file with symlink for '{}'", package.name);
                 fs::remove_file(&license_file)?;
-                create_symlink(&root_license.as_std_path(), &license_file)?;
+                create_relative_symlink(
+                    &root_license.as_std_path(),
+                    &license_file,
+                    &workspace.workspace_root.as_std_path(),
+                )?;
             }
         } else {
-            println!("creating license symlink for {}", package.name);
+            info!("creating license symlink for '{}'", package.name);
             let license_new_path = crate_dir.join(LICENSE_FILES[0]);
-            create_symlink(&root_license.as_std_path(), &license_new_path.as_std_path())?;
+            create_relative_symlink(
+                &root_license.as_std_path(),
+                &license_new_path.as_std_path(),
+                &workspace.workspace_root.as_std_path(),
+            )?;
         }
     }
 
@@ -47,7 +67,8 @@ pub fn run_licenses(_args: LicensesArgs) -> Result<()> {
 fn first_license_file(path: impl AsRef<Path>, license_files: &[&str]) -> Option<PathBuf> {
     for license_file in license_files {
         let path_to_license = path.as_ref().join(license_file);
-        if path_to_license.exists() {
+        info!("analyzing '{}'...", path_to_license.display());
+        if std::fs::read_link(&path_to_license).is_ok() {
             return Some(path_to_license);
         }
     }
@@ -55,14 +76,15 @@ fn first_license_file(path: impl AsRef<Path>, license_files: &[&str]) -> Option<
     None
 }
 
-#[cfg(unix)]
-fn create_symlink(src: &Path, dst: &Path) -> Result<()> {
-    std::os::unix::fs::symlink(src, dst)?;
-    Ok(())
-}
+fn create_relative_symlink(src: &Path, dst: &Path, workspace_root: &Path) -> Result<()> {
+    let relative_src = pathdiff::diff_paths(src, dst.parent().unwrap())
+        .ok_or_else(|| anyhow!("failed to create relative path"))?;
 
-#[cfg(windows)]
-fn create_symlink(src: &Path, dst: &Path) -> Result<()> {
-    std::os::windows::fs::symlink_file(src, dst)?;
+    #[cfg(unix)]
+    std::os::unix::fs::symlink(&relative_src, dst)?;
+
+    #[cfg(windows)]
+    std::os::windows::fs::symlink_file(&relative_src, dst)?;
+
     Ok(())
 }
