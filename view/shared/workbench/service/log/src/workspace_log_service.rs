18e765754c3d023ea381791e21ac48a55ae063fd
KrailUnknown <spore2102@gmail.com>
2024-09-19 19:23:15 +0200

    feat: TaoLogger skeleton

--- view/shared/workbench/service/log/src/workspace_log_service.rs
+++ view/shared/workbench/service/log/src/workspace_log_service.rs
@@ -1,6 +1,6 @@
 // use platform_log::AnyLogger;
 
-use platform_log::log_service::AnyLogger;
+use platform_log::log_service::{AnyLogger, LogLevel};
 
 pub struct WorkspaceLogService {
     file_logger: Box<dyn AnyLogger>,
@@ -19,5 +19,81 @@ impl WorkspaceLogService {
             buffer_logger,
             tao_logger,
         }
+    }
+}
+
+// fn init_custom_logging(app_handle: tauri::AppHandle) {
+//     struct TauriLogWriter {
+//         app_handle: tauri::AppHandle,
+//     }
+
+//     impl std::io::Write for TauriLogWriter {
+//         fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+//             let log_message = String::from_utf8_lossy(buf).to_string();
+//             let _ = self.app_handle.emit("logs-stream", log_message);
+//             Ok(buf.len())
+//         }
+
+//         fn flush(&mut self) -> std::io::Result<()> {
+//             Ok(())
+//         }
+//     }
+
+//     tracing_subscriber::registry()
+//         // log to stdout
+//         .with(tracing_subscriber::fmt::layer().with_writer(std::io::stdout))
+//         // log to frontend
+//         .with(
+//             tracing_subscriber::fmt::layer().with_writer(move || TauriLogWriter {
+//                 app_handle: app_handle.clone(),
+//             }),
+//         )
+//         .init();
+
+//     event!(tracing::Level::DEBUG, "Logging init");
+//     info!("Logging initialized");
+// }
+
+
+pub struct TaoLogger {
+    level: LogLevel,
+    formatter: Box<dyn Fn(&str, LogLevel) -> String>,
+} 
+
+impl AnyLogger for TaoLogger {
+    fn trace(&mut self, message: &str) {
+        todo!()
+    }
+
+    fn debug(&mut self, message: &str) {
+        todo!()
+    }
+
+    fn info(&mut self, message: &str) {
+        todo!()
+    }
+
+    fn warning(&mut self, message: &str) {
+        todo!()
+    }
+
+    fn error(&mut self, message: &str) {
+        todo!()
+    }
+
+    fn flush(&mut self) {
+        todo!()
+    }
+
+    fn set_level(&mut self, level: platform_log::log_service::LogLevel) {
+        todo!()
+    }
+
+    fn set_format(&mut self, formatter: Box<dyn Fn(&str, platform_log::log_service::LogLevel) -> String>) {
+        todo!()
+    }
+
+    fn is_ready(&self) -> bool {
+        todo!()
     }
 }
