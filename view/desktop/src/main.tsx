d040ea957388a5ac41b985da1d41326419db3474
Enthceph <enthceph@gmail.com>
2024-09-22 12:11:07 +0300

    feat: added redux

--- view/desktop/src/main.tsx
+++ view/desktop/src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/assets/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
