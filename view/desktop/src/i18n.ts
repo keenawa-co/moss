f46c71e072e40b97d42a72012f1274d81c523c44
Enthceph <enthceph@gmail.com>
2024-09-22 14:04:20 +0300

    chore: removed Settings props, turned off i18n logging

--- view/desktop/src/i18n.ts
+++ view/desktop/src/i18n.ts
@@ -5,7 +5,7 @@ import HttpApi from "i18next-http-backend";
 export const defaultNS = "ns1";
 
 i18next.use(initReactI18next).use(HttpApi).init({
-  debug: true,
+  debug: false,
   fallbackLng: "en",
   defaultNS,
 });
