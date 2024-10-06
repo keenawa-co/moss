8a4d7b6edf03102156050d219936873c92d56299
EnthusedFear <enthceph@gmail.com>
2024-10-06 14:10:18 +0300

    refactor: accordions list

--- view/desktop/src/components/LaunchPad.tsx
+++ view/desktop/src/components/LaunchPad.tsx
@@ -1,18 +1,16 @@
 import Accordion from "./DraggableAccordion";
 import SidebarHeader from "./SidebarHeader";
 import { useAppDispatch, RootState } from "@/store";
-import { IAccordion, setAccordions, setPreferredSize, setPreferredSizes } from "@/store/accordion/accordionSlice";
+import { setAccordions, setPreferredSizes } from "@/store/accordion/accordionSlice";
 import { useSelector } from "react-redux";
 import * as DesktopComponents from ".";
 import { useRef, useEffect } from "react";
-import { ImperativePanelGroupHandle, Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
-import { getPreferredSizeById } from "@/store/accordion/accordionSelectors";
 import { Resizable, ResizablePanel } from "./Resizable";
 import { AllotmentHandle } from "allotment";
 type DesktopComponentKeys = keyof typeof DesktopComponents;
 type OmittedComponents = Omit<
   Record<DesktopComponentKeys, any>,
-  "RootLayout" | "SidebarLayout" | "ContentLayout" | "PropertiesLayout"
+  "RootLayout" | "SidebarLayout" | "ContentLayout" | "PropertiesLayout" | "Button" | "SidebarHeader"
 >;
 type DesktopComponentsOmitted = keyof OmittedComponents;
 
@@ -20,8 +18,6 @@ const getDesktopComponentByName = (name: DesktopComponentsOmitted) => {
   if (!DesktopComponents[name]) return <div>{name}</div>;
 
   const Tag = DesktopComponents[name];
-  // FIXME: Type 'typeof Tag' is not assignable to type 'ElementType<any>'.
-  //@ts-ignore
   return <Tag />;
 };
 
@@ -29,19 +25,23 @@ const LaunchPad = () => {
   return (
     <>
       <SidebarHeader title="launchpad" />
-      <DroppableContainer />
+      <AccordionsList />
     </>
   );
 };
 
 export default LaunchPad;
 
-const DroppableContainer = () => {
+const AccordionsList = () => {
   const ref = useRef<AllotmentHandle>(null);
   const dispatch = useAppDispatch();
   const accordions = useSelector((state: RootState) => state.accordion.accordion);
   const preferredSizes = useSelector((state: RootState) => state.accordion.preferredSizes);
-  // const preferredSizes = useRef<number[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (!ref.current || preferredSizes.length === 0) return;
+    ref.current.resize(preferredSizes);
+  }, [accordions]);
 
   const toggleAccordion = (index: number) => {
     const updatedAccordions = accordions.map((accordion, i) =>
@@ -52,14 +52,8 @@ const DroppableContainer = () => {
   };
 
   const handleOnDragEnd = (sizes: number[]) => {
-    // preferredSizes.current = sizes;
     dispatch(setPreferredSizes(sizes));
   };
-
-  useEffect(() => {
-    if (!ref.current || preferredSizes.length === 0) return;
-    ref.current.resize(preferredSizes);
-  }, [accordions]);
 
   return (
     <div className="h-[calc(100%_-_42px)] overflow-auto">
@@ -68,14 +62,14 @@ const DroppableContainer = () => {
           <ResizablePanel
             key={accordion.id}
             minSize={accordion.isOpen ? 100 : 35}
-            maxSize={accordion.isOpen ? 300 : 35}
+            maxSize={accordion.isOpen ? Infinity : 35}
           >
             <Accordion {...accordion} index={index} handleClick={() => toggleAccordion(index)}>
               {getDesktopComponentByName(accordion.content as DesktopComponentsOmitted)}
             </Accordion>
           </ResizablePanel>
         ))}
-        <ResizablePanel>
+        <ResizablePanel minSize={0} maxSize={Infinity}>
           <span />
         </ResizablePanel>
       </Resizable>
