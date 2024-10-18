2a9d6295a4b3706b90ccaa4e5aa050597e1c5413
KrailUnknown <spore2102@gmail.com>
2024-10-18 07:06:33 +0200

    feat: added separate nixpkgs to handle the required surrealdb version

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        # Specific nixpkgs for surrealdb
+        surrealNixpkgs.url = "github:NixOS/nixpkgs/73b9e297fae10023e18162f0381330a1bd728037";
+        
         flake-utils.url = "github:numtide/flake-utils";
         rust-overlay = {
             url = "github:oxalica/rust-overlay";
@@ -10,37 +13,59 @@
         };
     };
 
-    outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    outputs = { self, nixpkgs, surrealNixpkgs, flake-utils, rust-overlay }:
         flake-utils.lib.eachDefaultSystem (system:
             let
                 pkgs = import nixpkgs {
                     inherit system;
                     overlays = [ (import rust-overlay) ];
                     config = {
-                        allowUnfree = true;    # Enable unfree packages
+                        allowUnfree = true;
                     };
                 };
 
-                surrealdbVersion = pkgs.surrealdb.overrideAttrs (old: {
-                    version = "1.5.4";   # Pin SurrealDB to version 1.5.4
-                });
+                # Import the specific nixpkgs for surrealdb
+                surrealPkgs = import surrealNixpkgs {
+                    inherit system;
+                    overlays = [ (import rust-overlay) ];
+                    config = {
+                        allowUnfree = true;    # Enable unfree packages if needed
+                    };
+                };
+
+                surrealdbVersion = surrealPkgs.surrealdb;
 
                 pnpmVersion = pkgs.pnpm.overrideAttrs (old: {
-                    version = "9.8.0";   # Pin pnpm to version 9.8.0
+                    version = "9.8.0";
+                    src = pkgs.fetchurl { # had to add this because of hash missmatches
+                        url = "https://registry.npmjs.org/pnpm/-/pnpm-9.8.0.tgz";
+                        sha256 = "Vqnna1F5bKf3O4XkTPg3EoYgkfTUmMDOTVt+zca6GPc=";
+                    }; 
                 });
 
-                rustToolchain = pkgs.rust-bin.stable."1.80.1".default;  # Pin Rust toolchain (rustc and cargo) to version 1.80.1
+                rustToolchain = pkgs.rust-bin.stable."1.80.1".default;
 
-                # Required tools and packages
-                packages = with pkgs; [
-                    webkitgtk
-                    gtk3
+                tauriDeps = with pkgs; [
+                    at-spi2-atk
+                    atkmm
                     cairo
                     gdk-pixbuf
                     glib
+                    gobject-introspection
+                    gobject-introspection.dev
+                    gtk3
+                    harfbuzz
+                    librsvg
+                    libsoup_3
+                    pango
+                    webkitgtk_4_1
+                    webkitgtk_4_1.dev
+                ];
+
+                # Required tools and packages
+                packages = with pkgs; [
                     dbus
                     openssl_3
-                    librsvg
                     curl
                     wget
                     pkg-config
@@ -48,8 +73,8 @@
                     cloc           
                     pnpmVersion             
                     rustToolchain    
-                    libsoup
-                ];
+                ] ++ tauriDeps;
+
             in
             {
                 devShells = {
