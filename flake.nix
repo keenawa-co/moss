829c6999cbcaec6341b4c49775e619e690224e2b
KrailUnknown <spore2102@gmail.com>
2024-10-24 06:03:55 +0200

    feat: attempt to make nix working by using example

--- flake.nix
+++ flake.nix
@@ -1,104 +1,93 @@
 {
-    description = "A flake to install tools required for working on keenawa-co/moss project";
-
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        # Specific nixpkgs for surrealdb
-        surrealNixpkgs.url = "github:NixOS/nixpkgs/73b9e297fae10023e18162f0381330a1bd728037";
-        
-        flake-utils.url = "github:numtide/flake-utils";
-        rust-overlay = {
-            url = "github:oxalica/rust-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    unstablePkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    surrealNixpkgs.url = "github:NixOS/nixpkgs/73b9e297fae10023e18162f0381330a1bd728037";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "unstablePkgs";
+      };
     };
+  };
+  outputs = { self, unstablePkgs, surrealNixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          unstable = import unstablePkgs {
+            inherit system overlays;
+          };
 
-    outputs = { self, nixpkgs, surrealNixpkgs, flake-utils, rust-overlay }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = import nixpkgs {
-                    inherit system;
-                    overlays = [ (import rust-overlay) ];
-                    config = {
-                        allowUnfree = true;
-                    };
+            # Import the specific nixpkgs for surrealdb
+            surrealPkgs = import surrealNixpkgs {
+                inherit system;
+                overlays = [ (import rust-overlay) ];
+                config = {
+                    allowUnfree = true;    # Enable unfree packages if needed
                 };
+            };
 
-                # Import the specific nixpkgs for surrealdb
-                surrealPkgs = import surrealNixpkgs {
-                    inherit system;
-                    overlays = [ (import rust-overlay) ];
-                    config = {
-                        allowUnfree = true;    # Enable unfree packages if needed
-                    };
-                };
+            surrealdbVersion = surrealPkgs.surrealdb;
 
-                surrealdbVersion = surrealPkgs.surrealdb;
+            pnpmVersion = unstable.pnpm.overrideAttrs (old: {
+                version = "9.8.0";
+                src = unstable.fetchurl { # had to add this because of hash missmatches
+                    url = "https://registry.npmjs.org/pnpm/-/pnpm-9.8.0.tgz";
+                    sha256 = "Vqnna1F5bKf3O4XkTPg3EoYgkfTUmMDOTVt+zca6GPc=";
+                }; 
+            });
 
-                pnpmVersion = pkgs.pnpm.overrideAttrs (old: {
-                    version = "9.8.0";
-                    src = pkgs.fetchurl { # had to add this because of hash missmatches
-                        url = "https://registry.npmjs.org/pnpm/-/pnpm-9.8.0.tgz";
-                        sha256 = "Vqnna1F5bKf3O4XkTPg3EoYgkfTUmMDOTVt+zca6GPc=";
-                    }; 
-                });
+            rustToolchain = unstable.rust-bin.stable."1.80.1".default;
 
-                rustToolchain = pkgs.rust-bin.stable."1.80.1".default;
+          common = with unstable; [
+            gtk3
+            glib
+            glib-networking
+            dbus
+            openssl_3
+            librsvg
+            gettext
+            libiconv
+            libsoup
+            libsoup_3
+            webkitgtk
+            webkitgtk_4_1
+            nodejs_20
+            corepack_20
+          ];
 
-                tauriDeps = with pkgs; [
-                    at-spi2-atk
-                    atkmm
-                    cairo
-                    gdk-pixbuf
-                    gobject-introspection
-                    gobject-introspection.dev
-                    gtk3
-                    harfbuzz
-                    librsvg
-                    libsoup_3
-                    pango
-                    webkitgtk_4_1
-                    webkitgtk_4_1.dev
-                    dbus
-                ];
+          # runtime Deps
+          libraries = with unstable;[
+            cairo
+            pango
+            harfbuzz
+            gdk-pixbuf
+            surrealdbVersion
+            libcanberra
+            libcanberra-gtk2
+            libcanberra-gtk3
+          ] ++ common;
 
-                # runtime Deps
-                libraries = with pkgs;[
-                    cairo
-                    pango
-                    harfbuzz
-                    gdk-pixbuf
-                    glib
-                    openssl_3
-                    libcanberra
-                    libcanberra-gtk2
-                    libcanberra-gtk3
-                ] ++ tauriDeps;
-
-                # Required tools and packages
-                packages = with pkgs; [
-                    curl
-                    wget
-                    surrealdbVersion
-                    cloc           
-                    pnpmVersion             
-                    rustToolchain    
-                ] ++ tauriDeps;
-
-            in
-            {
-                devShells = {
-                    default = pkgs.mkShell {
-                        buildInputs = packages;
-
-                        shellHook = ''
-                            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-                            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-                            export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
-                            '';
-                    };
-                };
-
-            }
-        );
+          # compile-time deps
+          packages = with unstable; [
+            pnpmVersion
+            curl
+            wget
+            pkg-config
+            rustToolchain
+          ] ++ common;
+        in
+        {
+          devShells.default = unstable.mkShell {
+            nativeBuildInputs = packages;
+            buildInputs = libraries;
+            shellHook = ''
+              export LD_LIBRARY_PATH=${unstable.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${unstable.gsettings-desktop-schemas}/share/gsettings-schemas/${unstable.gsettings-desktop-schemas.name}:${unstable.gtk3}/share/gsettings-schemas/${unstable.gtk3.name}:$XDG_DATA_DIRS
+              export GIO_MODULE_DIR="${unstable.glib-networking}/lib/gio/modules/"
+            '';
+          };
+        }
+      );
 }
