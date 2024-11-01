d5d285205c55f076aa469f744691f13c912ec479
EnthusedFear <enthceph@gmail.com>
2024-11-01 10:18:47 +0200

    refactori: HeadBar

--- view/desktop/src/components/HeadBar/HeadBarButton.tsx
+++ view/desktop/src/components/HeadBar/HeadBarButton.tsx
@@ -1,20 +1,22 @@
-import { Icon, Icons } from "@repo/ui";
+import { cn, Icon, Icons } from "@repo/ui";
 import { ComponentPropsWithoutRef } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 interface HeadBarButtonProps extends ComponentPropsWithoutRef<"button"> {
   icon: Icons;
   label?: string;
-  sortableId?: string | number; // Renamed to avoid conflict
+  iconClassName?: string;
+  sortableId?: string | number;
 }
 
 export const HeadBarButton = ({ icon, label, sortableId = -1, ...props }: HeadBarButtonProps) => {
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: sortableId });
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: sortableId,
+  });
 
   const style = {
     transform: CSS.Translate.toString(transform),
     transition,
-    zIndex: 50,
   };
 
   return (
@@ -22,12 +24,18 @@ export const HeadBarButton = ({ icon, label, sortableId = -1, ...props }: HeadBa
       ref={setNodeRef}
       {...attributes}
       {...listeners}
-      className="group flex  items-center gap-1.5 font-normal transition-colors"
       style={style}
       {...props}
+      className={cn(
+        "group z-40 flex items-center gap-1.5 rounded-[3px] font-normal transition-colors hover:bg-[#EBECF0]",
+        {
+          "z-50 cursor-grabbing opacity-50": isDragging == true,
+        },
+        props.className
+      )}
     >
-      <Icon icon={icon} className="size-[18px] text-[#525252] group-hover:text-[#0065FF] group-active:text-[#0747A6]" />
-      {label && <span className="text-[#161616] group-hover:text-[#0065FF] group-active:text-[#0747A6]">{label}</span>}
+      <Icon icon={icon} className={cn("group-active:text-black size-[18px] text-[#525252]", props.iconClassName)} />
+      {label && <span className="group-active:text-black text-ellipsis text-[#161616]">{label}</span>}
     </button>
   );
 };
