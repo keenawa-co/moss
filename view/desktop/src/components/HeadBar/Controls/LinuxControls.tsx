31b089874ac32e9bc2d806b6fabe44923e231fc5
Enthceph <enthceph@gmail.com>
2024-11-02 17:55:20 +0200

    refactor: changed back tauri windows provider

--- view/desktop/src/components/HeadBar/Controls/LinuxControls.tsx
+++ view/desktop/src/components/HeadBar/Controls/LinuxControls.tsx
@@ -1,39 +1,32 @@
-import { useState, type HTMLProps } from "react";
+import { useContext, type HTMLProps } from "react";
 import { ControlButton } from "./ControlButton";
 import { Icons } from "./icons";
 import { cn } from "@repo/ui";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+import ControlsContext from "./ControlsContext";
 
 export function LinuxControls({ className, ...props }: HTMLProps<HTMLDivElement>) {
-  const apiWindow = getCurrentWindow();
-  const [isMaximized, setIsMaximized] = useState(false);
-
-  const handleToggleMaximize = async () => {
-    const isMaximized = await apiWindow.isMaximized();
-    setIsMaximized(isMaximized);
-    await apiWindow.toggleMaximize();
-  };
+  const { isWindowMaximized, minimizeWindow, maximizeWindow, closeWindow } = useContext(ControlsContext);
 
   return (
     <div className={cn("mr-[10px] flex h-auto items-center space-x-[13px]", className)} {...props}>
       <ControlButton
-        onClick={() => apiWindow.minimize()}
+        onClick={minimizeWindow}
         className="m-0 h-6 w-6 cursor-default rounded-full bg-[rgba(var(--color-window-controls-linux-background))] p-0 text-[rgba(var(--color-window-controls-linux-text))] hover:bg-[rgba(var(--color-window-controls-linux-hover-background))] active:bg-[rgba(var(--color-window-controls-linux-active-background))] dark:bg-[#373737] dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
         <Icons.minimizeWin className="h-[9px] w-[9px]" />
       </ControlButton>
       <ControlButton
-        onClick={handleToggleMaximize}
+        onClick={maximizeWindow}
         className="m-0 h-6 w-6 cursor-default rounded-full bg-[rgba(var(--color-window-controls-linux-background))] p-0 text-[rgba(var(--color-window-controls-linux-text))] hover:bg-[rgba(var(--color-window-controls-linux-hover-background))] active:bg-[rgba(var(--color-window-controls-linux-active-background))] dark:bg-[#373737] dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
-        {isMaximized ? (
-          <Icons.maximizeWin className="h-2 w-2" />
-        ) : (
+        {isWindowMaximized ? (
           <Icons.maximizeRestoreWin className="h-[9px] w-[9px]" />
+        ) : (
+          <Icons.maximizeWin className="h-2 w-2" />
         )}
       </ControlButton>
       <ControlButton
-        onClick={() => apiWindow.close()}
+        onClick={closeWindow}
         className="m-0 h-6 w-6 cursor-default rounded-full bg-[rgba(var(--color-window-controls-linux-background))] p-0 text-[rgba(var(--color-window-controls-linux-text))] hover:bg-[rgba(var(--color-window-controls-linux-hover-background))] active:bg-[rgba(var(--color-window-controls-linux-active-background))] dark:bg-[#373737] dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
         <Icons.closeWin className="h-2 w-2" />
--- view/desktop/src/components/HeadBar/Controls/WindowsControls.tsx
+++ view/desktop/src/components/HeadBar/Controls/WindowsControls.tsx
@@ -1,41 +1,34 @@
-import { useState, type HTMLProps } from "react";
+import { useContext, type HTMLProps } from "react";
 import { ControlButton } from "./ControlButton";
 import { Icons } from "./icons";
 import { cn } from "@repo/ui";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+import ControlsContext from "./ControlsContext";
 
 // FIXME: fix opacity bg-[rgba(var(--color-windows-close-button-background))]/90
 // FIXME: analyze and fix dark:...
 
 export function WindowsControls({ className, ...props }: HTMLProps<HTMLDivElement>) {
-  const apiWindow = getCurrentWindow();
-  const [isMaximized, setIsMaximized] = useState(false);
-
-  const handleToggleMaximize = async () => {
-    const isMaximized = await apiWindow.isMaximized();
-    setIsMaximized(isMaximized);
-    await apiWindow.toggleMaximize();
-  };
+  const { isWindowMaximized, minimizeWindow, maximizeWindow, closeWindow } = useContext(ControlsContext);
 
   return (
     <div className={cn("flex h-full", className)} {...props}>
       <ControlButton
-        onClick={() => apiWindow.minimize()}
+        onClick={minimizeWindow}
         className="h-full w-[46px] cursor-default rounded-none bg-transparent text-[rgba(var(--color-primary))]/90 hover:bg-[#0000000d] active:bg-[rgba(var(--color-primary))]/[.03]  dark:text-white dark:hover:bg-white/[.06] dark:active:bg-white/[.04]"
       >
         <Icons.minimizeWin />
       </ControlButton>
       <ControlButton
-        onClick={handleToggleMaximize}
+        onClick={maximizeWindow}
         className={cn(
           "h-full w-[46px] cursor-default rounded-none bg-transparent",
           "text-[rgba(var(--color-primary))]/90 hover:bg-[#0000000d] active:bg-[rgba(var(--color-primary))]/[.03] dark:text-white dark:hover:bg-white/[.06] dark:active:bg-white/[.04]"
         )}
       >
-        {isMaximized ? <Icons.maximizeWin /> : <Icons.maximizeRestoreWin />}
+        {isWindowMaximized ? <Icons.maximizeRestoreWin /> : <Icons.maximizeWin />}
       </ControlButton>
       <ControlButton
-        onClick={() => apiWindow.close()}
+        onClick={closeWindow}
         className="h-full w-[46px] cursor-default rounded-none bg-transparent text-[rgba(var(--color-primary))]/90 hover:bg-[rgba(var(--color-windows-close-button-background))] hover:text-white active:bg-[rgba(var(--color-windows-close-button-background))]/90 dark:text-white"
       >
         <Icons.closeWin />
