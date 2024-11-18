00ce35512a646fe13943236bb1ef3611c484bb1b
EnthusedFear <enthceph@gmail.com>
2024-11-18 18:50:08 +0200

    style: ActionsGroup paddings

--- view/shared/ui/src/Menu/Item.tsx
+++ view/shared/ui/src/Menu/Item.tsx
@@ -11,33 +11,36 @@ export type ItemProps = ScopedProps<ComponentPropsWithoutRef<typeof MenuPrimitiv
   disabled?: boolean;
   icon?: Icons;
   hideIcon?: boolean;
+  iconClassName?: string;
 };
 
-export const Item = forwardRef<ItemElement, ItemProps>(({ className, hideIcon = false, ...props }, forwardedRef) => {
-  return (
-    <MenuPrimitive.Item
-      {...props}
-      ref={forwardedRef}
-      className={cn(
-        "flex items-center gap-1.5 rounded py-0.5 pl-2 pr-3",
-        {
-          "cursor-not-allowed opacity-50": props.disabled,
-          "cursor-pointer hover:bg-[#D4E2FF] hover:outline-none": !props.disabled,
-        },
-        className
-      )}
-    >
-      {!hideIcon &&
-        (props.icon ? (
-          <Icon icon={props.icon} className="text-[#8D8D8D]" />
-        ) : (
-          <Icon icon="Documentation" className="opacity-0" />
-        ))}
-      <div className="flex w-full items-center gap-2.5">
-        <span>{props.label}</span>
+export const Item = forwardRef<ItemElement, ItemProps>(
+  ({ iconClassName, className, hideIcon = false, ...props }, forwardedRef) => {
+    return (
+      <MenuPrimitive.Item
+        {...props}
+        ref={forwardedRef}
+        className={cn(
+          "flex items-center gap-1.5 rounded py-0.5 pl-[7px] pr-[11px]",
+          {
+            "cursor-not-allowed opacity-50": props.disabled,
+            "cursor-pointer hover:bg-[#D4E2FF] hover:outline-none": !props.disabled,
+          },
+          className
+        )}
+      >
+        {!hideIcon &&
+          (props.icon ? (
+            <Icon icon={props.icon} className="text-[#8D8D8D]" />
+          ) : (
+            <Icon icon="Documentation" className="opacity-0" />
+          ))}
+        <div className="flex w-full items-center gap-2.5">
+          <span>{props.label}</span>
 
-        {props.shortcut && <div className="ml-auto text-[#8D8D8D]">{props.shortcut.join("")}</div>}
-      </div>
-    </MenuPrimitive.Item>
-  );
-});
+          {props.shortcut && <div className="ml-auto text-[#8D8D8D]">{props.shortcut.join("")}</div>}
+        </div>
+      </MenuPrimitive.Item>
+    );
+  }
+);
