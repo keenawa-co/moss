9aa2a58c80b0b06e8dcef7a2f0852352764a7838
g10z3r <20g10z3r@gmail.com>
2024-11-23 13:42:08 +0100

    Added a new selector `bgc`

--- view/shared/config-tailwind/tailwind.config.ts
+++ view/shared/config-tailwind/tailwind.config.ts
@@ -1,10 +1,26 @@
 import { mergeConfig } from "../ui/src/tailwind/mergeConfig";
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = mergeConfig({
   content: ["../ui/src/**/*.{js,ts,jsx,tsx,css}"],
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          bgc: (value: string) => ({
+            background: value,
+          }),
+        },
+        {
+          // Allow the use of arbitrary values
+          values: theme("colors"),
+          type: ["color", "any"], // Permit any color values
+        }
+      );
+    }),
+  ],
 });
 
 export default config;
