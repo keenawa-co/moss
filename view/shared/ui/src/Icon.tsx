305f93a2e7c538027ac165a4cb8f5d8635af56bf
g10z3r <20g10z3r@gmail.com>
2024-11-23 21:09:41 +0100

    Seems like I fixed the import error

--- view/shared/ui/src/Icon.tsx
+++ view/shared/ui/src/Icon.tsx
@@ -4,37 +4,41 @@ import { cn } from "./utils/utils";
 
 export type Icons = keyof typeof icons;
 
-export const Icon = ({
-  icon,
-  className,
-  ...props
-}: {
+interface IconProps extends ComponentPropsWithoutRef<"svg"> {
   icon: Icons | null;
   className?: string;
-} & ComponentPropsWithoutRef<"svg">) => {
-  const IconTag = icons[icon];
+}
 
-  if (!IconTag) {
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className={cn("size-4 flex-shrink-0", className)}>
-        <path fill="#ff69b4" d="M0 0h4v4H0z" />
-        <path d="M4 0h4v4H4z" />
-        <path fill="#ff69b4" d="M8 0h4v4H8z" />
-        <path d="M12 0h4v4h-4zM0 4h4v4H0z" />
-        <path fill="#ff69b4" d="M4 4h4v4H4z" />
-        <path d="M8 4h4v4H8z" />
-        <path fill="#ff69b4" d="M12 4h4v4h-4zM0 8h4v4H0z" />
-        <path d="M4 8h4v4H4z" />
-        <path fill="#ff69b4" d="M8 8h4v4H8z" />
-        <path d="M12 8h4v4h-4zM0 12h4v4H0z" />
-        <path fill="#ff69b4" d="M4 12h4v4H4z" />
-        <path d="M8 12h4v4H8z" />
-        <path fill="#ff69b4" d="M12 12h4v4h-4z" />
-      </svg>
-    );
+export const Icon = ({ icon, className, ...props }: IconProps) => {
+  // Check that the icon is not null and exists in icons
+  if (icon && icons[icon]) {
+    const IconTag = icons[icon];
+    return <IconTag className={cn("size-4 flex-shrink-0 text-[var(--color-primary)]", className)} {...props} />;
   }
 
-  return <IconTag className={cn("size-4 flex-shrink-0 text-[var(--color-primary)]", className)} {...props} />;
+  // If the icon is null or not found, return the default SVG
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      className={cn("size-4 flex-shrink-0", className)}
+      {...props}
+    >
+      <path fill="#ff69b4" d="M0 0h4v4H0z" />
+      <path d="M4 0h4v4H4z" />
+      <path fill="#ff69b4" d="M8 0h4v4H8z" />
+      <path d="M12 0h4v4h-4zM0 4h4v4H0z" />
+      <path fill="#ff69b4" d="M4 4h4v4H4z" />
+      <path d="M8 4h4v4H8z" />
+      <path fill="#ff69b4" d="M12 4h4v4h-4zM0 8h4v4H0z" />
+      <path d="M4 8h4v4H4z" />
+      <path fill="#ff69b4" d="M8 8h4v4H8z" />
+      <path d="M12 8h4v4h-4zM0 12h4v4H0z" />
+      <path fill="#ff69b4" d="M4 12h4v4H4z" />
+      <path d="M8 12h4v4H8z" />
+      <path fill="#ff69b4" d="M12 12h4v4h-4z" />
+    </svg>
+  );
 };
 
 export default Icon;
