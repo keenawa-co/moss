c7d1dd3e9700283fece64346e779c59ae54f09ab
EnthusedFear <enthceph@gmail.com>
2024-11-30 14:34:04 +0200

    fix: windows min button hover

--- view/desktop/src/components/parts/HeadBar/Controls/WindowsControls.tsx
+++ view/desktop/src/components/parts/HeadBar/Controls/WindowsControls.tsx
@@ -1,8 +1,10 @@
 import { useContext, type HTMLProps } from "react";
-import { ControlButton } from "./ControlButton";
-import { ControlsIcons } from "./icons";
+
 import { cn } from "@repo/moss-ui";
+
+import { ControlButton } from "./ControlButton";
 import ControlsContext from "./ControlsContext";
+import { ControlsIcons } from "./icons";
 
 // FIXME: fix opacity bg-[rgba(var(--color-windows-close-button-background))]/90
 // FIXME: analyze and fix dark:...
@@ -14,7 +16,7 @@ export function WindowsControls({ className, ...props }: HTMLProps<HTMLDivElemen
     <div className={cn("flex h-full", className)} {...props}>
       <ControlButton
         onClick={minimizeWindow}
-        className="text-[var(--color-primary)]/90 active:background-[var(--color-primary)]/[.03] h-full w-[46px] cursor-default rounded-none bg-transparent hover:bg-[#0000000]  dark:text-white dark:hover:bg-white/[.06] dark:active:bg-white/[.04]"
+        className="text-[var(--color-primary)]/90 active:background-[var(--color-primary)]/[.03] h-full w-[46px] cursor-default rounded-none bg-transparent hover:bg-[#0000000d]  "
       >
         <ControlsIcons.minimizeWin />
       </ControlButton>
@@ -22,14 +24,14 @@ export function WindowsControls({ className, ...props }: HTMLProps<HTMLDivElemen
         onClick={maximizeWindow}
         className={cn(
           "h-full w-[46px] cursor-default rounded-none bg-transparent",
-          "text-[var(--color-primary)]/90 active:background-[var(--color-primary)]/[.03] hover:bg-[#0000000d] dark:text-white dark:hover:bg-white/[.06] dark:active:bg-white/[.04]"
+          "text-[var(--color-primary)]/90 active:background-[var(--color-primary)]/[.03] hover:bg-[#0000000d]"
         )}
       >
         {isWindowMaximized ? <ControlsIcons.maximizeRestoreWin /> : <ControlsIcons.maximizeWin />}
       </ControlButton>
       <ControlButton
         onClick={closeWindow}
-        className="text-[var(--color-primary)]/90 active:background-[var(--color-windows-close-button-background)]/90 h-full w-[46px] cursor-default rounded-none bg-transparent hover:text-white hover:background-[var(--color-windows-close-button-background)] dark:text-white"
+        className="text-[var(--color-primary)]/90 active:background-[var(--color-windows-close-button-background)]/90 h-full w-[46px] cursor-default rounded-none bg-transparent hover:text-white hover:background-[var(--color-windows-close-button-background)]"
       >
         <ControlsIcons.closeWin />
       </ControlButton>
