ac1bb80fb77c3ce87ef3353b50fce0629821c969
Hongyu Yang <brutusyhy@gmail.com>
2024-12-01 21:59:02 +0300

    feat(moss-typebridge): Tuple, Nested Type, Custom Type and serde(rename) parsing
    1. Implement parsing of Tuple type
    2. Implement parsing of nested types
    3. Implement parsing of custom type references
    4. Implement parsing of `serde(rename)` attribute
    5. Code restructure

--- crates/moss-typebridge/src/main.rs
+++ crates/moss-typebridge/src/main.rs
@@ -1,7 +1,10 @@
 use moss_typebridge_macro::type_bridge;
 use serde::Serialize;
 
-#[type_bridge]
+#[type_bridge(language = "TypeScript", output_path = "CustomEnum.ts")]
+enum CustomEnum {}
+
+#[type_bridge(language = "TypeScript", output_path = "TestStruct.ts")]
 struct TestStruct {
     #[serde(rename = "Integer")]
     integer: i32,
@@ -12,6 +15,9 @@ struct TestStruct {
     string: String,
     option: Option<String>,
     vector: Vec<i32>,
+    tuple: (String, i32, bool),
+    complex: Vec<(Option<String>, i32)>,
+    custom_enum: CustomEnum,
 }
 
 pub fn main() {}
