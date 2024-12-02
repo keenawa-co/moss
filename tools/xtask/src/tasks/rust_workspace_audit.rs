e0b94270d1aafbde909a58ebe9ee0206863a01fd
Hongyu Yang <brutusyhy@gmail.com>
2024-12-02 14:42:19 +0300

    chore: Add `fail-fast` flag for xtask workspace audit
    With the `fail-fast` flag when running workspace dependencies audit, the program will terminate as soon as any error (like rule violation) is encountered. Because many tasks are relatively light, however, several errors might still appear in the console log before the program stops.

--- tools/xtask/src/tasks/rust_workspace_audit.rs
+++ tools/xtask/src/tasks/rust_workspace_audit.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use futures::future::join_all;
 use smol::fs;
 use std::{io, sync::Arc};
+use tokio::task::JoinSet;
 use toml::Value;
 use tracing::{error, info, trace, warn};
 
@@ -18,16 +19,19 @@ pub struct RustWorkspaceAuditCommandArgs {
 pub async fn check_dependencies_job(
     args: RustWorkspaceAuditCommandArgs,
     metadata: Metadata,
+    fail_fast: bool,
 ) -> Result<()> {
+    let mut task_set = JoinSet::new();
+
     let config_file = Arc::new(ConfigFile::load(&args.config_file_path).await?);
-    let tasks = metadata
+    metadata
         .packages
         .into_iter()
         .filter(|p| metadata.workspace_members.contains(&p.id))
-        .map(|package| {
+        .for_each(|package| {
             let config_file_clone = Arc::clone(&config_file);
 
-            tokio::task::spawn(async move {
+            task_set.spawn(async move {
                 trace!("analyzing '{}'...", package.name);
                 let cargo_toml_content = match fs::read_to_string(&package.manifest_path).await {
                     Ok(content) => content,
@@ -51,20 +55,40 @@ pub async fn check_dependencies_job(
                     }
                 };
 
-                handle_package_dependencies(cargo_toml, &config_file_clone, package).await;
+                match handle_package_dependencies(cargo_toml, &config_file_clone, package).await {
+                    Ok(()) => Ok(()),
+                    Err(e) => Err(e),
+                }
+            });
+        });
 
-                Ok(())
-            })
-        })
-        .collect::<Vec<_>>();
-
-    for result in join_all(tasks).await {
+    while let Some(result) = task_set.join_next().await {
         match result {
             Ok(Ok(())) => {}
-            Ok(Err(e)) => error!("Error processing package: {}", e),
-            Err(e) => error!("Task panicked: {}", e),
+            Ok(Err(e)) => {
+                if fail_fast {
+                    task_set.abort_all();
+                    return Err(anyhow!(e));
+                }
+                error!("Error when processing package: {}", e)
+            }
+            Err(e) => {
+                if fail_fast {
+                    task_set.abort_all();
+                    return Err(anyhow!("Task panicked: {}", e));
+                }
+                error!("Task panicked: {}", e)
+            }
         }
     }
+
+    // for result in join_all(tasks).await {
+    //     match result {
+    //         Ok(Ok(())) => {}
+    //         Ok(Err(e)) => error!("Error processing package: {}", e),
+    //         Err(e) => error!("Task panicked: {}", e),
+    //     }
+    // }
 
     Ok(())
 }
@@ -73,7 +97,7 @@ async fn handle_package_dependencies(
     cargo_toml: Value,
     ignored_deps: &ConfigFile,
     package: Package,
-) {
+) -> Result<()> {
     if let Some(dependencies) = cargo_toml.get("dependencies").and_then(|d| d.as_table()) {
         for (dep_name, dep_value) in dependencies {
             if let Some(ignored_list) = ignored_deps.rust_workspace_audit.ignore.get(&package.name)
@@ -95,7 +119,13 @@ async fn handle_package_dependencies(
                     "crate '{}' has non-workspace dependency: {}",
                     package.name, dep_name
                 );
+                return Err(anyhow!(
+                    "crate '{}' has non-workspace dependency: {}",
+                    package.name,
+                    dep_name
+                ));
             }
         }
     }
+    Ok(())
 }
