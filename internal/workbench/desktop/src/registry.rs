e4c96d8086037c38bda4d9f641fb522066faf01d
g10z3r <20g10z3r@gmail.com>
2024-12-03 19:22:56 +0100

    Draft registry

--- internal/workbench/desktop/src/registry.rs
+++ internal/workbench/desktop/src/registry.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use desktop_models::{
     actions::MenuItem,
     view::{GroupId, TreeViewDescriptor, TreeViewGroup, TreeViewGroupLocation},
@@ -5,8 +6,8 @@ use desktop_models::{
 use hashbrown::HashMap;
 use moss_text::ReadOnlyStr;
 use parking_lot::RwLock;
-use std::fmt::Debug;
 use std::sync::Arc;
+use std::{any::TypeId, fmt::Debug};
 
 pub struct MenuRegistry {
     menus: HashMap<ReadOnlyStr, Vec<MenuItem>>,
@@ -100,16 +101,138 @@ impl ViewsRegistry {
     }
 }
 
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+use std::rc::Rc;
+
+/// Marker structure for type erasure.
+struct Abstract(());
+
+/// Represents a saved command with type information erased to `Abstract`.
+struct Command {
+    /// Pointer to the stored function.
+    func_ptr: NonNull<Abstract>,
+    /// Function to invoke the stored function.
+    invoke_fn: unsafe fn(
+        NonNull<Abstract>, // func_ptr
+        *mut (),           // args_ptr
+        *mut (),           // result_ptr
+    ),
+    /// Function to release the stored function.
+    drop_fn: unsafe fn(NonNull<Abstract>),
+    /// Prevents implementation of `Send` and `Sync`.
+    _not_send_sync: PhantomData<Rc<()>>,
+}
+
+impl Drop for Command {
+    fn drop(&mut self) {
+        unsafe {
+            (self.drop_fn)(self.func_ptr);
+        }
+    }
+}
+
+pub struct CommandRegistry {
+    commands: HashMap<u32, Command>,
+}
+
+impl CommandRegistry {
+    fn new() -> Self {
+        CommandRegistry {
+            commands: HashMap::new(),
+        }
+    }
+
+    /// Registers a new command with a unique identifier.
+    pub fn register<F, Args, Ret>(&mut self, id: u32, func: F)
+    where
+        F: Fn(Args) -> Ret + 'static,
+        Args: 'static,
+        Ret: 'static,
+    {
+        // Wrap the function in a `Box` and obtain a raw pointer.
+        let boxed_func = Box::new(func);
+        let func_ptr =
+            unsafe { NonNull::new_unchecked(Box::into_raw(boxed_func) as *mut Abstract) };
+
+        // Unsafe function to invoke the stored function.
+        unsafe fn invoke_fn<F, Args, Ret>(
+            func_ptr: NonNull<Abstract>,
+            args_ptr: *mut (),
+            result_ptr: *mut (),
+        ) where
+            F: Fn(Args) -> Ret,
+            Args: 'static,
+            Ret: 'static,
+        {
+            let func = &*(func_ptr.as_ptr() as *const F);
+            // Restore arguments and free memory.
+            let args_box = Box::from_raw(args_ptr as *mut Args);
+            let args = *args_box;
+            // Invoke the function.
+            let result = func(args);
+            // Store the result.
+            std::ptr::write(result_ptr as *mut Ret, result);
+        }
+
+        // Unsafe function to release the stored function.
+        unsafe fn drop_fn<F>(func_ptr: NonNull<Abstract>) {
+            let _ = Box::from_raw(func_ptr.as_ptr() as *mut F);
+        }
+
+        let command = Command {
+            func_ptr,
+            invoke_fn: invoke_fn::<F, Args, Ret>,
+            drop_fn: drop_fn::<F>,
+            _not_send_sync: PhantomData,
+        };
+
+        self.commands.insert(id, command);
+    }
+
+    /// Invokes a registered command with the given identifier and arguments.
+    pub fn invoke<Args, Ret>(&self, id: u32, args: Args) -> Result<Ret, &'static str>
+    where
+        Args: 'static,
+        Ret: 'static,
+    {
+        let command = self.commands.get(&id).ok_or("Command not found")?;
+
+        // Wrap the arguments and obtain a raw pointer.
+        let boxed_args = Box::new(args);
+        let args_ptr = Box::into_raw(boxed_args) as *mut ();
+
+        // Allocate space for the result.
+        let mut result = std::mem::MaybeUninit::<Ret>::uninit();
+
+        // Invoke the function.
+        unsafe {
+            (command.invoke_fn)(command.func_ptr, args_ptr, result.as_mut_ptr() as *mut ());
+        }
+
+        // Restore the result.
+        let result = unsafe { result.assume_init() };
+
+        Ok(result)
+    }
+}
+
 pub struct RegistryManager {
     pub views: Arc<RwLock<ViewsRegistry>>,
     pub menus: Arc<RwLock<MenuRegistry>>,
+    pub commands: Arc<RwLock<CommandRegistry>>,
 }
 
 impl RegistryManager {
     pub fn new() -> Self {
         let views = Arc::new(RwLock::new(ViewsRegistry::new()));
         let menus = Arc::new(RwLock::new(MenuRegistry::new()));
+        let commands = Arc::new(RwLock::new(CommandRegistry::new()));
 
-        Self { views, menus }
+        Self {
+            views,
+            menus,
+            commands,
+        }
     }
 }
