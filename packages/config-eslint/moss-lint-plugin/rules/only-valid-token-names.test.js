95c84e546683abf30654ef2db443a1fdf312ddaa
EnthusedFear <enthceph@gmail.com>
2024-12-05 17:19:57 +0200

    test: added tests for only-valid-token-names eslint rule

--- packages/config-eslint/moss-lint-plugin/rules/only-valid-token-names.test.js
+++ packages/config-eslint/moss-lint-plugin/rules/only-valid-token-names.test.js
@@ -17,19 +17,125 @@ const ruleTester = new RuleTester({
 ruleTester.run("only-valid-token-names", rule, {
   valid: [
     {
-      name: "Valid selector in string",
-      code: `<div className="bg-[--color-sideBar-background]"></div>`,
+      name: "Valid token in selector",
+      code: `<div className="background-[--color-primary]"></div>`,
+    },
+    {
+      name: "Valid token selector in template string ",
+      code: `<div className={\`background-[--color-primary]\`}></div>`,
+    },
+    {
+      name: "Valid token in selector with var()",
+      code: `<div className="background-[var(--color-primary)]"></div>`,
+    },
+    {
+      name: "Valid token in selector with pseudoclass",
+      code: `<div className="hover:text-[--color-primary]"></div>`,
+    },
+    {
+      name: "Valid token with group selector",
+      code: `<div className="group-text-[--color-primary]"></div>`,
+    },
+    {
+      name: "Valid tailwind arbitrary values",
+      code: `<div className="before:content-['Festivus'] top-[117px]"></div>`,
+    },
+    {
+      name: "Valid tailwind arbitrary properties",
+      code: `<div className="[mask-type:luminance] lg:[--scroll-offset:44px]"></div>`,
+    },
+    {
+      name: "Valid tailwind arbitrary variant",
+      code: `<div className="lg:[&:nth-child(3)]:hover:underline"></div>`,
     },
   ],
   invalid: [
     {
-      name: "Invalid selector in string",
-      code: `<div className="bg-[--invalid-selector]"></div>`,
+      name: "Invalid selector in template string",
+      code: `<div className={\`background-[--invalid-value]\`}></div>`,
       errors: [
         {
           messageId: "invalidTokenName",
           data: {
-            tokenName: "--invalid-selector",
+            tokenName: "--invalid-value",
+          },
+        },
+      ],
+    },
+    {
+      name: "Invalid selector in string",
+      code: `<div className="background-[--invalid-value]"></div>`,
+      errors: [
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-value",
+          },
+        },
+      ],
+    },
+    {
+      name: "Invalid selector in string with var()",
+      code: `<div className="background-[var(--invalid-value)]"></div>`,
+      errors: [
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-value",
+          },
+        },
+      ],
+    },
+    {
+      name: "Invalid token in selector with pseudoclass",
+      code: `<div className="hover:text-[--invalid-value]"></div>`,
+      errors: [
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-value",
+          },
+        },
+      ],
+    },
+    {
+      name: "Invalid token with group selector",
+      code: `<div className="group-text-[--invalid-value]"></div>`,
+      errors: [
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-value",
+          },
+        },
+      ],
+    },
+    {
+      name: "Invalid tokens",
+      code: `<div className="text-[--very-long-invalid-value] text-[var(--invalid-value2)] hover:text-[--custom-val] group-bg-[--invalid-group-value]"></div>`,
+      errors: [
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--very-long-invalid-value",
+          },
+        },
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-value2",
+          },
+        },
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--custom-val",
+          },
+        },
+        {
+          messageId: "invalidTokenName",
+          data: {
+            tokenName: "--invalid-group-value",
           },
         },
       ],
