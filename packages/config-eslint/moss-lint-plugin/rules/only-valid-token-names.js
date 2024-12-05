e00b485490a36f97b27ef541b6af3fd951a533f4
EnthusedFear <enthceph@gmail.com>
2024-12-05 17:19:17 +0200

    refactor: only-valid-token-names refactoring

--- packages/config-eslint/moss-lint-plugin/rules/only-valid-token-names.js
+++ packages/config-eslint/moss-lint-plugin/rules/only-valid-token-names.js
@@ -6,62 +6,39 @@ const colorKeysToCssValues = (colors) => {
   return keys.map((color) => "--color-" + color.replaceAll(".", "-"));
 };
 
-const VALID_TOKENS = colorKeysToCssValues(themeInterface.colors);
-const SELECTOR_WITH_ARBITRARY_VALUE = /(?:group-)?\w+-\[(?:var\((--[\w-]+)\)|(--[\w-]+))\]/g;
-const ARBITRARY_VALUE = /\[(?:var\((--[\w-]+)\)|(--[\w-]+))\]/;
+const VALID_TOKENS = new Set(colorKeysToCssValues(themeInterface.colors));
 
-const getInvalidTokenName = (str) => {
-  const selectors = str.match(SELECTOR_WITH_ARBITRARY_VALUE);
-  if (selectors === null || selectors.length === 0) return false;
+const ANY_TW_SELECTOR_WITH_ARBITRARY_VALUE = /\b[\w|\-:]+\[(?:var\((--[\w-]+)\)|(--[\w-]+))\]/g;
 
-  const tokens = selectors.map((className) => {
-    const selector = className.match(ARBITRARY_VALUE);
-    const token = selector[1] || selector[2];
-    return token;
-  });
+const getAllInvalidTokens = (str, loc) => {
+  const invalidTokens = [];
+  let arr;
 
-  return tokens.find((token) => !VALID_TOKENS.includes(token));
-};
+  while ((arr = ANY_TW_SELECTOR_WITH_ARBITRARY_VALUE.exec(str)) !== null) {
+    const className = arr[0];
+    const name = arr[1] || arr[2];
 
-const hasInvalidTokens = (str) => {
-  const selectors = str.match(SELECTOR_WITH_ARBITRARY_VALUE);
+    const startColumn = loc.start.column + str.indexOf(className) + 1;
+    const endColumn = startColumn + className.length;
 
-  if (selectors === null || selectors.length === 0) return false;
+    if (!VALID_TOKENS.has(name)) {
+      invalidTokens.push({
+        name,
+        loc: {
+          start: {
+            line: loc.start.line,
+            column: startColumn,
+          },
+          end: {
+            line: loc.end.line,
+            column: endColumn,
+          },
+        },
+      });
+    }
+  }
 
-  return selectors.some((className) => {
-    const selector = className.match(ARBITRARY_VALUE);
-    const token = selector[1] || selector[2];
-
-    return !VALID_TOKENS.includes(token);
-  });
-};
-
-const getInvalidSelectorLoc = (str, loc) => {
-  const selectors = str.match(SELECTOR_WITH_ARBITRARY_VALUE);
-  if (selectors === null || selectors.length === 0) return false;
-
-  const invalidSelector = selectors.find((className) => {
-    const selector = className.match(ARBITRARY_VALUE);
-    const token = selector[1] || selector[2];
-
-    return !VALID_TOKENS.includes(token);
-  });
-
-  if (invalidSelector === undefined) return loc;
-
-  const startColumn = loc.start.column + str.indexOf(invalidSelector) + 1;
-  const endColumn = startColumn + invalidSelector.length;
-
-  return {
-    start: {
-      line: loc.start.line,
-      column: startColumn,
-    },
-    end: {
-      line: loc.start.line,
-      column: endColumn,
-    },
-  };
+  return invalidTokens;
 };
 
 /**  @type {import('eslint').Rule.RuleModule} **/
@@ -81,26 +58,34 @@ export default {
   create(context) {
     return {
       Literal(node) {
-        if (node.value && typeof node.value === "string" && hasInvalidTokens(node.value)) {
-          context.report({
-            node,
-            messageId: "invalidTokenName",
-            data: {
-              tokenName: getInvalidTokenName(node.value),
-            },
-            loc: getInvalidSelectorLoc(node.value, node.loc),
+        if (typeof node.value === "string") {
+          const invalidTokens = getAllInvalidTokens(node.value, node.loc);
+
+          invalidTokens.forEach((token) => {
+            context.report({
+              node,
+              messageId: "invalidTokenName",
+              data: {
+                tokenName: token.name,
+              },
+              loc: token.loc,
+            });
           });
         }
       },
       TemplateElement(node) {
-        if (node.value && typeof node.value.raw === "string" && hasInvalidTokens(node.value.raw)) {
-          context.report({
-            node,
-            messageId: "invalidTokenName",
-            data: {
-              tokenName: getInvalidTokenName(node.value),
-            },
-            loc: getInvalidSelectorLoc(node.value.raw, node.loc),
+        if (typeof node.value.raw === "string") {
+          const invalidTokens = getAllInvalidTokens(node.value.raw, node.loc);
+
+          invalidTokens.forEach((token) => {
+            context.report({
+              node,
+              messageId: "invalidTokenName",
+              data: {
+                tokenName: token.name,
+              },
+              loc: token.loc,
+            });
           });
         }
       },
