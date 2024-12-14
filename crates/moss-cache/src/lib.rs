284df9fee6e26352748d828454fc6997a672fae8
Hongyu Yang <brutusyhy@gmail.com>
2024-12-14 16:19:34 +0300

    refactor(moss-cache): various refactorings
    1. Remove `/experiments` folder
    2. Remove unnecessary `Moss` prefix
    3. Remove `MossCacheBuilder`
    4. Remove unnecessary comments
    5. Change the return type of `deregister` to a Result with a NonexistentKey error when deregistering a key without registered update function.

--- crates/moss-cache/src/lib.rs
+++ crates/moss-cache/src/lib.rs
@@ -1,5 +1,5 @@
 use dashmap::DashMap;
-use moka::sync::{Cache, CacheBuilder};
+use moka::sync::{Cache as MokaCache, CacheBuilder as MokaCacheBuilder};
 use moka::Expiry;
 use std::any::{type_name, Any};
 use std::fmt::Debug;
@@ -10,7 +10,7 @@ use thiserror::Error;
 type DynType = Arc<dyn Any + Send + Sync>;
 
 #[derive(Debug, Error)]
-pub enum MossCacheError {
+pub enum CacheError {
     #[error("The value of key '{key}' does not have type '{type_name}'")]
     TypeMismatch { key: String, type_name: String },
 
@@ -19,19 +19,19 @@ pub enum MossCacheError {
 }
 
 #[derive(Clone, Debug)]
-struct MossCacheItem {
+struct CacheItem {
     pub item: DynType,
     ttl: Duration,
 }
 
-impl MossCacheItem {
-    pub(crate) fn get<T>(&self, key: &str) -> Result<Arc<T>, MossCacheError>
+impl CacheItem {
+    pub(crate) fn get<T>(&self, key: &str) -> Result<Arc<T>, CacheError>
     where
         T: Any + Send + Sync,
     {
         match self.item.clone().downcast::<T>() {
             Ok(result) => Ok(result),
-            Err(_) => Err(MossCacheError::TypeMismatch {
+            Err(_) => Err(CacheError::TypeMismatch {
                 key: key.to_string(),
                 type_name: type_name::<T>().to_string(),
             }),
@@ -40,11 +40,11 @@ impl MossCacheItem {
 }
 
 pub struct CustomExpiry {}
-impl Expiry<String, MossCacheItem> for CustomExpiry {
+impl Expiry<String, CacheItem> for CustomExpiry {
     fn expire_after_create(
         &self,
         key: &String,
-        value: &MossCacheItem,
+        value: &CacheItem,
         created_at: Instant,
     ) -> Option<Duration> {
         Some(value.ttl)
@@ -53,7 +53,7 @@ impl Expiry<String, MossCacheItem> for CustomExpiry {
     fn expire_after_read(
         &self,
         key: &String,
-        value: &MossCacheItem,
+        value: &CacheItem,
         read_at: Instant,
         duration_until_expiry: Option<Duration>,
         last_modified_at: Instant,
@@ -62,70 +62,55 @@ impl Expiry<String, MossCacheItem> for CustomExpiry {
     }
 }
 
-impl MossCacheItem {
-    fn new(item: DynType, ttl: Duration) -> MossCacheItem {
-        MossCacheItem { item, ttl }
+impl CacheItem {
+    fn new(item: DynType, ttl: Duration) -> CacheItem {
+        CacheItem { item, ttl }
     }
 }
 
-type DynamicTypeCache = Cache<String, MossCacheItem>;
+type DynamicTypeCache = MokaCache<String, CacheItem>;
 
-pub struct MossCacheBuilder {
-    cache_builder: CacheBuilder<String, MossCacheItem, DynamicTypeCache>,
+pub struct Cache {
+    cache: MokaCache<String, CacheItem>,
+    retrievers: DashMap<String, Arc<dyn Fn() -> CacheItem>>,
 }
 
-impl MossCacheBuilder {
-    pub fn new(max_capacity: u64) -> MossCacheBuilder {
-        MossCacheBuilder {
-            cache_builder: CacheBuilder::new(max_capacity).expire_after(CustomExpiry {}),
-        }
-    }
-
-    pub fn build(self) -> MossCache {
-        MossCache {
-            cache: self.cache_builder.build(),
+impl Cache {
+    pub fn new(max_capacity: u64) -> Cache {
+        Cache {
+            cache: MokaCacheBuilder::new(max_capacity)
+                .expire_after(CustomExpiry {})
+                .build(),
             retrievers: DashMap::new(),
         }
     }
-}
 
-pub struct MossCache {
-    cache: Cache<String, MossCacheItem>,
-    retrievers: DashMap<String, Arc<dyn Fn() -> MossCacheItem>>,
-}
-
-impl MossCache {
     pub fn contains(&self, key: &str) -> bool {
-        /// Check if there's stored cache value for key
         self.cache.contains_key(key)
     }
     pub fn insert<T>(&self, key: &str, value: T, ttl: Duration)
     where
         T: Any + Send + Sync,
     {
-        /// Insert a key-value pair into the cache with a given TTL
         self.cache
-            .insert(key.to_string(), MossCacheItem::new(Arc::new(value), ttl));
+            .insert(key.to_string(), CacheItem::new(Arc::new(value), ttl));
     }
 
     pub fn delete(&self, key: &str) {
-        /// Deletes the stored value for key
         self.cache.remove(key);
     }
 
-    pub fn get<T>(&self, key: &str) -> Result<Arc<T>, MossCacheError>
+    pub fn get<T>(&self, key: &str) -> Result<Arc<T>, CacheError>
     where
         T: Any + Send + Sync,
     {
-        /// Attempt to first get the value of the key; if not, it will first update the cache value
-        /// using the registered update function
         match self.cache.get(key) {
             Some(cache_item) => cache_item.get::<T>(key),
             None => {
                 let retriever = self
                     .retrievers
                     .get(key)
-                    .ok_or(MossCacheError::NonexistentKey {
+                    .ok_or(CacheError::NonexistentKey {
                         key: key.to_string(),
                     })?
                     .value()
@@ -137,18 +122,13 @@ impl MossCache {
         }
     }
 
-    pub fn take<T>(&self, key: &str) -> Result<T, MossCacheError>
+    pub fn take<T>(&self, key: &str) -> Result<T, CacheError>
     where
         T: Any + Clone + Send + Sync,
     {
-        /// Removes the stored value for key and get the raw T value
-        /// To get T from Arc<T>, T must be Clone
-        let cache_item = self
-            .cache
-            .remove(key)
-            .ok_or(MossCacheError::NonexistentKey {
-                key: key.to_string(),
-            })?;
+        let cache_item = self.cache.remove(key).ok_or(CacheError::NonexistentKey {
+            key: key.to_string(),
+        })?;
         cache_item
             .get::<T>(key)
             .map(|arc_t| Arc::<T>::unwrap_or_clone(arc_t))
@@ -158,11 +138,10 @@ impl MossCache {
     where
         T: Any + Send + Sync,
     {
-        /// Register a key with an update function, which will be called when the entry is expired
         let update = Arc::new(update);
         let retriever = {
             let update = Arc::clone(&update);
-            Arc::new(move || MossCacheItem::new(Arc::new(update()), ttl))
+            Arc::new(move || CacheItem::new(Arc::new(update()), ttl))
         };
         self.retrievers.insert(key.to_string(), retriever.clone());
         self.cache
@@ -171,12 +150,14 @@ impl MossCache {
             .unwrap()
     }
 
-    pub fn deregister(&self, key: &str) -> Option<()> {
-        /// Delete cached value and the registered function
-        /// If no function has been registered for this key, returns None
-        /// Otherwise returns Some(())
+    pub fn deregister(&self, key: &str) -> Result<(), CacheError> {
         self.cache.invalidate(key);
-        self.retrievers.remove(key).map(|_| ())
+        match self.retrievers.remove(key) {
+            Some(retriever) => Ok(()),
+            None => Err(CacheError::NonexistentKey {
+                key: key.to_string(),
+            }),
+        }
     }
 }
 
@@ -185,14 +166,14 @@ mod test {
     use std::thread;
     #[test]
     fn test_cache_insert() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert("i32", 32i32, Duration::from_millis(1000));
         assert_eq!(*cache.get::<i32>("i32").unwrap(), 32);
     }
 
     #[test]
     fn test_cache_delete() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert("i32", 32i32, Duration::from_millis(1000));
         cache.delete("i32");
         assert!(!cache.contains("i32"));
@@ -200,14 +181,14 @@ mod test {
 
     #[test]
     fn test_cache_delete_nonexistent() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.delete("nonexistent");
         assert!(!cache.contains("nonexistent"));
     }
 
     #[test]
     fn test_cache_get() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert("i32", 32i32, Duration::from_millis(1000));
         assert_eq!(*cache.get::<i32>("i32").unwrap(), 32);
         assert!(cache.contains("i32"));
@@ -216,14 +197,14 @@ mod test {
     #[test]
     #[should_panic]
     fn test_cache_get_incorrect_type() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert("i32", 32i32, Duration::from_millis(1000));
         cache.get::<String>("i32").unwrap();
     }
 
     #[test]
     fn test_cache_take() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert("i32", 32i32, Duration::from_millis(1000));
         let i: i32 = cache.take::<i32>("i32").unwrap();
         assert_eq!(i, 32);
@@ -232,7 +213,7 @@ mod test {
 
     #[test]
     fn test_cache_expiry() {
-        let cache = MossCacheBuilder::new(1024).build();
+        let cache = Cache::new(1024);
         cache.insert::<i32>("i32", 32i32, Duration::from_millis(500));
         thread::sleep(Duration::from_millis(1000));
         assert!(!cache.contains("i32"));
@@ -240,11 +221,22 @@ mod test {
 
     #[test]
     fn test_cache_register() {
-        let cache_builder = MossCacheBuilder::new(1024);
-        let cache = cache_builder.build();
+        let cache = Cache::new(1024);
         cache.register::<i32>("i32", Duration::from_millis(500), || 32);
         assert_eq!(*cache.get::<i32>("i32").unwrap(), 32);
         thread::sleep(Duration::from_millis(1000));
         assert_eq!(*cache.get::<i32>("i32").unwrap(), 32);
+    }
+
+    #[test]
+    fn example() {
+        let cache = Cache::new(1024);
+        cache.register::<String>("file", Duration::from_millis(100), || {
+            std::fs::read_to_string("test.txt").unwrap()
+        });
+        println!("{:?}", cache.get::<String>("file"));
+        thread::sleep(Duration::from_millis(1000));
+        assert!(!cache.contains("file"));
+        assert!(cache.get::<String>("file").is_ok());
     }
 }
