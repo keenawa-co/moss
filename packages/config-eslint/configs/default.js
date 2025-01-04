941a53ed1dde7969aa12c17264ea1131022775ed
EnthusedFear <enthceph@gmail.com>
2025-01-04 12:38:47 +0200

    chore: removed react-refresh eslint plugin

--- packages/config-eslint/configs/default.js
+++ packages/config-eslint/configs/default.js
@@ -1,5 +1,4 @@
 import reactHooksPlugin from "eslint-plugin-react-hooks";
-import reactRefreshPlugin from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 import mossLintPlugin from "../moss-lint-plugin/index.js";
@@ -30,7 +29,6 @@ export default tseslint.config(...tseslint.configs.recommended, {
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
