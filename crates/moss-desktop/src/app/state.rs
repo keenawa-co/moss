c7508354c8be51431bebd40377478386d1e8d3fb
g10z3r <20g10z3r@gmail.com>
2025-01-08 17:48:15 +0100

    Changing the default theme to light

--- crates/moss-desktop/src/app/state.rs
+++ crates/moss-desktop/src/app/state.rs
@@ -41,8 +41,8 @@ impl AppStateManager {
             },
             defaults: AppDefaults {
                 theme: ThemeDescriptor {
-                    id: "theme-defaults.MossDarkDefault".to_string(),
-                    name: "Moss Dark Default".to_string(),
+                    id: "theme-defaults.MossLightDefault".to_string(),
+                    name: "Moss Light Default".to_string(),
                     source: BUILTIN_ADDONS_DIR
                         .join("theme-defaults")
                         .join("themes")
