b9e08ba6018266eca0e8578fddcd356f0afd2bae
EnthusedFear <enthceph@gmail.com>
2025-01-09 09:07:09 +0200

    merged some hooks

--- view/desktop/src/hooks/useColorTheme.ts
+++ view/desktop/src/hooks/useColorTheme.ts
@@ -1,6 +1,14 @@
+import { getColorThemes } from "@/api/appearance";
 import { invokeMossCommand } from "@/lib/backend/platfrom";
 import { ThemeDescriptor } from "@repo/moss-desktop";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useGetColorThemes = () => {
+  return useQuery<ThemeDescriptor[], Error>({
+    queryKey: ["getColorTheme"],
+    queryFn: getColorThemes,
+  });
+};
 
 const changeTheme = async (themeDescriptor: ThemeDescriptor): Promise<void> => {
   await invokeMossCommand("workbench.changeColorTheme", {
--- view/desktop/src/hooks/useLanguagePack.ts
+++ view/desktop/src/hooks/useLanguagePack.ts
@@ -1,6 +1,14 @@
+import { getLanguagePacks } from "@/api/appearance";
 import { invokeMossCommand } from "@/lib/backend/platfrom";
 import { LocaleDescriptor } from "@repo/moss-desktop";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useGetLanguagePacks = () => {
+  return useQuery<LocaleDescriptor[], Error>({
+    queryKey: ["getLanguagePacks"],
+    queryFn: getLanguagePacks,
+  });
+};
 
 const changeLanguagePack = async (localeDescriptor: LocaleDescriptor): Promise<void> => {
   await invokeMossCommand("workbench.changeLanguagePack", {
