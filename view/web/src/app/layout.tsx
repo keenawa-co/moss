7c7bb496c887d4f5ece091e66c6b6a244da07c60
EnthusedFear <enthceph@gmail.com>
2025-01-10 15:06:04 +0200

    removed comment

--- view/web/src/app/layout.tsx
+++ view/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import "@repo/moss-ui/src/styles.css"; // ESlint error:Unable to resolve path to module '@repo/moss-ui/styles.css'.eslintimport/no-unresolved
+import "@repo/moss-ui/src/styles.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
