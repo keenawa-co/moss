ae67f6e03ade9997f76e7136c2c1755a4dc3ffe8
jevgenis33 <eugene230189@gmail.com>
2025-01-12 16:00:09 +0100

    fix: fixinf ci unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const config: JestConfigWithTsJest = {
   collectCoverageFrom: ["<rootDir>/packages/*/src/**/*.{js,jsx,ts,tsx}"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/packages/*/src/__tests__/"],
   coverageDirectory: "coverage",
-  testResultsProcessor: "jest-sonar-reporter",
+  //testResultsProcessor: "jest-sonar-reporter",
 };
 
 export default config;
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,5 +1,5 @@
 import { env } from "process";
-import { defineWorkspace } from "vitest/config";
+import { configDefaults, defineWorkspace } from "vitest/config";
 
 import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
 
@@ -25,7 +25,14 @@ export default defineWorkspace([
   {
     test: {
       name: "packages",
-      include: ["packages/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+      include: [
+        "packages/config-eslint/*.{test,spec}.?(c|m)[jt]s?(x)",
+        "packages/config-tailwind/*.{test,spec}.?(c|m)[jt]s?(x)",
+        "packages/config-typescript/*.{test,spec}.?(c|m)[jt]s?(x)",
+        "packages/moss_lang/*.{test,spec}.?(c|m)[jt]s?(x)",
+        "packages/moss-ui/*.{test,spec}.?(c|m)[jt]s?(x)",
+      ],
+      exclude: [...configDefaults.exclude],
     },
   },
   {
