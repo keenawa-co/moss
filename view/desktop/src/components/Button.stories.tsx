98f5e912baac34cd6ee16b6916de314d948ee49b
EnthusedFear <enthceph@gmail.com>
2025-02-05 21:00:17 +0200

    added dark theme for button

--- view/desktop/src/components/Button.stories.tsx
+++ view/desktop/src/components/Button.stories.tsx
@@ -8,112 +8,132 @@ const sizes = ["xs", "sm", "md", "lg", "xl"] as const;
 const colors = {
   primary: {
     solid: {
-      bg: "linear-gradient(in oklab, rgb(59, 130, 246) 0%, rgb(37, 99, 235) 100%)",
-      border: "#1d4ed8",
+      background: "linear-gradient(in oklab, rgb(59, 130, 246) 0%, rgb(37, 99, 235) 100%)",
+      border: null,
       text: "white",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#1d4ed8 0px 0px 0px 1px",
     },
     outlined: {
-      bg: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(239, 246, 255) 100%)",
-      border: "#bfdbfe",
+      background: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(239, 246, 255) 100%)",
+      border: null,
       text: "#1e40af",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#bfdbfe 0px 0px 0px 1px",
     },
     soft: {
-      bg: "#dbeafe",
-      border: "#dbeafe",
+      background: "#dbeafe",
+      border: null,
       text: "#1d4ed8",
+      boxShadow: "#dbeafe 0px 0px 0px 1px",
     },
     ghost: {
-      bg: "#dbeafe",
+      background: "#dbeafe",
       border: "#dbeafe",
       text: "#1d4ed8",
+      boxShadow: null,
     },
   },
   warning: {
     solid: {
-      bg: "linear-gradient(rgb(250, 204, 21), rgb(234, 179, 8))",
-      border: "#ca8a04",
+      background: "linear-gradient(rgb(250, 204, 21), rgb(234, 179, 8))",
+      border: null,
       text: "#422006",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#ca8a04 0px 0px 0px 1px",
     },
     outlined: {
-      bg: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(254, 252, 232) 100%)",
-      border: "#fef08a",
+      background: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(254, 252, 232) 100%)",
+      border: null,
       text: "#854d0e",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#fef08a 0px 0px 0px 1px",
     },
     soft: {
-      bg: "rgb(254, 249, 195)",
-      border: "rgb(254, 249, 195)",
+      background: "rgb(254, 249, 195)",
+      border: null,
       text: "#a16207",
+      boxShadow: "rgb(254,249,195) 0px 0px 0px 1px",
     },
     ghost: {
-      bg: "rgb(254, 249, 195)",
+      background: "rgb(254, 249, 195)",
       border: "rgb(254, 249, 195)",
       text: "#a16207",
+      boxShadow: null,
     },
   },
   success: {
     solid: {
-      bg: "linear-gradient(rgb(34, 197, 94), rgb(22, 163, 74))",
-      border: "#15803d",
+      background: "linear-gradient(rgb(34, 197, 94), rgb(22, 163, 74))",
+      border: null,
       text: "white",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#15803d 0px 0px 0px 1px",
     },
     outlined: {
-      bg: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(220, 252, 231) 100%)",
-      border: "#bbf7d0",
+      background: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(220, 252, 231) 100%)",
+      border: null,
       text: "rgb(22, 101, 52)",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#bbf7d0 0px 0px 0px 1px",
     },
     soft: {
-      bg: "rgb(220, 252, 231)",
+      background: "rgb(220, 252, 231)",
       border: "rgb(220, 252, 231)",
       text: "rgb(21, 128, 61)",
+      boxShadow: "rgb(220, 252, 231) 0px 0px 0px 1px",
     },
     ghost: {
-      bg: "rgb(220, 252, 231)",
+      background: "rgb(220, 252, 231)",
       border: "rgb(220, 252, 231)",
       text: "rgb(21, 128, 61)",
+      boxShadow: null,
     },
   },
   danger: {
     solid: {
-      bg: "linear-gradient(rgb(239, 68, 68), rgb(220, 38, 38))",
-      border: "#b91c1c",
+      background: "linear-gradient(rgb(239, 68, 68), rgb(220, 38, 38))",
+      border: null,
       text: "white",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#b91c1c 0px 0px 0px 1px",
     },
     outlined: {
-      bg: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(254, 242, 242) 100%)",
-      border: "#fdcfcf",
+      background: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(254, 242, 242) 100%)",
+      border: null,
       text: "rgb(153, 27, 27)",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#fdcfcf 0px 0px 0px 1px",
     },
     soft: {
-      bg: "rgb(254, 226, 226)",
-      border: "rgb(254, 226, 226)",
+      background: "rgb(254, 226, 226)",
+      border: null,
       text: "rgb(185, 28, 28)",
+      boxShadow: "rgb(254, 226, 226) 0px 0px 0px 1px",
     },
     ghost: {
-      bg: "rgb(254, 226, 226)",
+      background: "rgb(254, 226, 226)",
       border: "rgb(254, 226, 226)",
       text: "rgb(185, 28, 28)",
+      boxShadow: null,
     },
   },
   neutral: {
     solid: {
-      bg: "linear-gradient(rgb(113, 113, 122), rgb(82, 82, 91))",
-      border: "#3f3f46",
+      background: "linear-gradient(rgb(113, 113, 122), rgb(82, 82, 91))",
+      border: null,
       text: "white",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#3f3f46 0px 0px 0px 1px",
     },
     outlined: {
-      bg: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(250, 250, 250) 100%)",
-      border: "#e4e4e7",
+      background: "radial-gradient(76% 151% at 52% -52%, rgba(255, 255, 255, 0.6) 0%, rgb(250, 250, 250) 100%)",
+      border: null,
       text: "rgb(39, 39, 42)",
+      boxShadow: "rgba(255,255,255,0.25) 0px 1px 0px 0px inset,#e4e4e7 0px 0px 0px 1px",
     },
     soft: {
-      bg: "rgb(244, 244, 245)",
-      border: "rgb(244, 244, 245)",
+      background: "rgb(244, 244, 245)",
+      border: null,
       text: "rgb(39, 39, 42)",
+      boxShadow: "rgb(244, 244, 245) 0px 0px 0px 1px",
     },
     ghost: {
-      bg: "rgb(244, 244, 245)",
+      background: "rgb(244, 244, 245)",
       border: "rgb(244, 244, 245)",
       text: "rgb(39, 39, 42)",
+      boxShadow: null,
     },
   },
 } as const;
@@ -126,7 +146,7 @@ const convertColorsToCSSVars = (
   for (const [category, variants] of Object.entries(colors)) {
     for (const [variant, properties] of Object.entries(variants)) {
       for (const [prop, value] of Object.entries(properties)) {
-        const cssVarName = `--color-button-${category}-${variant}-${prop}`.replace(/bg/g, "background");
+        const cssVarName = `--color-button-${category}-${variant}-${prop}`;
         cssVars[cssVarName] = value;
       }
     }
@@ -144,7 +164,7 @@ const meta: Meta = {
   },
   decorators: [
     (Story) => (
-      <div style={convertColorsToCSSVars(colors)} className="text-(--moss-primary)">
+      <div className="text-(--moss-primary)">
         <Story />
       </div>
     ),
@@ -175,30 +195,39 @@ export const Primary: Story = {
 export const Intents: Story = {
   render: () => {
     return (
-      <table className="border-separate border-spacing-2">
-        <tr>
-          <th />
-          {variants.map((variant) => {
-            return <th className="text-left capitalize">{variant}</th>;
+      <>
+        <div>
+          <div className="flex">
+            <div className="text-black dark:text-[#101012]">Light theme</div>
+            <div className="text-white dark:text-white">Dark theme</div>
+          </div>
+        </div>
+
+        <table className="border-separate border-spacing-2">
+          <tr>
+            <th />
+            {variants.map((variant) => {
+              return <th className="text-left capitalize">{variant}</th>;
+            })}
+          </tr>
+          {(Object.keys(colors) as Array<keyof typeof colors>).map((intent) => {
+            return (
+              <tr key={intent}>
+                <th className="text-left capitalize">{intent}</th>
+                {variants.map((variant) => {
+                  return (
+                    <td key={variant}>
+                      <Button.Root intent={intent} variant={variant}>
+                        <Button.Label>Button</Button.Label>
+                      </Button.Root>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
           })}
-        </tr>
-        {(Object.keys(colors) as Array<keyof typeof colors>).map((intent) => {
-          return (
-            <tr key={intent}>
-              <th className="text-left capitalize">{intent}</th>
-              {variants.map((variant) => {
-                return (
-                  <td key={variant}>
-                    <Button.Root intent={intent} variant={variant}>
-                      <Button.Label>Button</Button.Label>
-                    </Button.Root>
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </table>
+        </table>
+      </>
     );
   },
 };
